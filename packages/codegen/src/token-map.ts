28612b761dae8768f4a43c10d84df23e5f5fc475
Segun Adebayo <joseshegs@gmail.com>
2022-07-27 23:16:27 +0400

    chore: update token map

--- packages/codegen/src/token-map.ts
+++ packages/codegen/src/token-map.ts
@@ -2,28 +2,33 @@ import type { Tokens } from '@css-panda/types';
 import { traverse } from 'object-traversal';
 import { isString } from 'lodash';
 
-export function getColorsMap(colors: Tokens['colors']) {
+export function getColorsMap(values: Tokens['colors']) {
   const map = new Map<string, string>();
-  traverse(colors, function walk(ctx) {
+
+  traverse(values, (ctx) => {
     const { depth, nodePath } = ctx.meta;
+
     const single = depth === 1 && isString(ctx.value);
     const nested = depth > 1;
+
     if ((single || nested) && nodePath) {
-      map.set(nodePath, ctx.value);
+      map.set(`colors.${nodePath}`, ctx.value);
     }
   });
+
   return map;
 }
 
 export function getFontSizeMap(values: Tokens['fontSizes']) {
   const map = new Map<string, string>();
+
   traverse(
     values,
-    function walk(ctx) {
+    (ctx) => {
       const { nodePath } = ctx.meta;
       if (!nodePath) return;
       map.set(
-        nodePath,
+        `fontSizes.${nodePath}`,
         isString(ctx.value) ? { fontSize: ctx.value } : ctx.value
       );
     },
@@ -33,16 +38,32 @@ export function getFontSizeMap(values: Tokens['fontSizes']) {
   return map;
 }
 
-export function getTokenMap(values: Record<string, string | number>) {
+export function getSpacingMap(values: Tokens['spacing']) {
+  return getTokenMap('spacing', values, true);
+}
+
+export function getTokenMap(
+  category: string,
+  values: Record<string, string | number>,
+  negate = false
+) {
   const map = new Map<string, string>();
+
   traverse(
     values,
-    function walk(ctx) {
+    (ctx) => {
       const { nodePath } = ctx.meta;
       if (!nodePath) return;
-      map.set(nodePath, ctx.value);
+
+      const pre = category ? `${category}.` : '';
+      map.set(`${pre}${nodePath}`, ctx.value);
+
+      if (negate) {
+        map.set(`${pre}-${nodePath}`, `-${ctx.value}`);
+      }
     },
     { maxDepth: 1 }
   );
+
   return map;
 }
