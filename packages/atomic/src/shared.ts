3d3eabc3e4840a1ea4a7ad754e959c4287db4a2b
Segun Adebayo <joseshegs@gmail.com>
2022-08-02 22:33:39 +0400

    Move breakpoint utils to package

--- packages/atomic/src/shared.ts
+++ packages/atomic/src/shared.ts
@@ -6,9 +6,3 @@ export function tap<T>(value: T, cb: (value: T) => void) {
   cb(value);
   return value;
 }
-
-export function toPx(value: number | string | null): string | null {
-  if (value == null) return null;
-  const unitless = parseFloat(value.toString()) == value;
-  return unitless || typeof value === 'number' ? `${value}px` : value;
-}
