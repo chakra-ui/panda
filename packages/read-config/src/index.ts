c8ffae797b4fb3eaea5e4f3083c10d64ab119af1
anubra266 <anubra266@gmail.com>
2022-08-05 13:07:47 +0100

    chore: update read config package

--- packages/read-config/src/index.ts
+++ packages/read-config/src/index.ts
@@ -1,16 +1,17 @@
 import { loadConfig } from 'unconfig';
 
-type ConfigType = any;
+type ConfigType = Record<string, any>;
 
 export class Config {
   constructor(root?: string) {
     this.root = root;
+    this.config = undefined;
   }
 
   private root: string | undefined;
-  private config: ConfigType;
+  private config: ConfigType | undefined;
 
-  private resolveConfig() {
+  public resolveConfig() {
     return loadConfig({
       sources: [
         {
@@ -23,20 +24,36 @@ export class Config {
     });
   }
 
-  public load(callback: (config: ConfigType) => void) {
-    Promise.resolve(this.resolveConfig()).then(({ config }) => {
-      this.config = config;
-      callback(config);
-    });
-  }
-
-  public async loadSync() {
-    const { config } = await this.resolveConfig();
-    this.config = config;
-    return config;
+  public load() {
+    return Promise.resolve(this.resolveConfig());
   }
 
   public readConfig() {
     return this.config;
+  }
+
+  public get(path: string) {
+    if (!this.config) return undefined;
+    const keys = path.split('.');
+    const value = keys.reduce((acc, nxt) => acc?.[nxt], this.config);
+    return value;
+  }
+
+  public set(path: string, value: any) {
+    if (!this.config) return undefined;
+    const keys = path.split('.');
+    let obj = this.config;
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      const next = keys[i + 1];
+      if (next === undefined) {
+        obj[key] = value;
+        break;
+      }
+      obj = obj[key];
+    }
+
+    this.config = obj;
+    return obj;
   }
 }
