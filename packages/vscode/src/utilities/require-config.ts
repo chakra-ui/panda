ae4f93167a720c970dd53ae6e76bb8261efa5b78
anubra266 <anubra266@gmail.com>
2022-08-05 13:08:13 +0100

    chore: use @css-panda/read-config in vscode extension

--- packages/vscode/src/utilities/require-config.ts
+++
@@ -1,9 +0,0 @@
-import * as tsImport from "ts-import";
-
-export function requireModule(module: string) {
-  const res = tsImport.loadSync(module, {
-    mode: tsImport.LoadMode.Compile,
-    compileOptions: {},
-  }).default;
-  return res;
-}
