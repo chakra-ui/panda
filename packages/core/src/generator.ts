072d61dae0de0625b954694ca53827d5ac4503a8
Segun Adebayo <joseshegs@gmail.com>
2022-08-22 17:42:58 +0400

    Improve config dependency watching

--- packages/core/src/generator.ts
+++ packages/core/src/generator.ts
@@ -1,15 +1,22 @@
-import { debug } from '@css-panda/logger'
+import { createDebugger, info } from '@css-panda/logger'
 import { loadConfigFile } from '@css-panda/read-config'
 import path from 'path'
 import { contentWatcher } from './content-watcher'
 import { createContext } from './create-context'
 import { generateSystem } from './generators'
+import { getCommonDir } from './get-common-dir'
 import { tempWatcher } from './temp-watcher'
 import { createWatcher } from './watcher'
 
+const debug = createDebugger('generator')
+
+process.setMaxListeners(Infinity)
+
 export async function generator(genOpts: { outdir: string; content: string[]; cwd: string }) {
   const { outdir, content, cwd } = genOpts
+
   const fixtureDir = path.dirname(require.resolve('@css-panda/fixture'))
+
   let { config, code, dependencies } = await loadConfigFile({
     root: path.join(fixtureDir, 'src'),
   })
@@ -18,23 +25,39 @@ export async function generator(genOpts: { outdir: string; content: string[]; cw
     throw new Error('💥 No config found')
   }
 
-  debug('config', config.prefix)
-
   const ctx = createContext(config)
 
-  await generateSystem(ctx, { outdir, config: code, clean: true })
+  await generateSystem(ctx, {
+    outdir,
+    configCode: code,
+    clean: true,
+  })
+
+  info('⚙️ generated system')
 
   const tmp = await tempWatcher(ctx, { outdir, cwd })
-  await contentWatcher(ctx, { content, cwd, outdir, tmpdir: tmp.dir })
+  const _content = await contentWatcher(ctx, { content, cwd, outdir, tmpdir: tmp.dir })
 
   if (!dependencies) return
 
-  const configWatcher = createWatcher(['fixture/src/*.ts'], {
-    cwd: '/Users/abrahamaremu/Dev/personal/chakra-ui/panda-css/css-panda/packages',
-  })
+  const commonDir = getCommonDir(dependencies)
+
+  debug.extend('config:dependencies', ' > ')(commonDir)
+
+  const configWatcher = createWatcher(
+    dependencies.map((file) => file.replace(commonDir, '')),
+    { cwd: getCommonDir(dependencies) },
+  )
+
+  async function close() {
+    await configWatcher.close()
+    await tmp.watcher.close()
+    await _content.watcher.close()
+  }
 
   configWatcher.on('update', async () => {
-    await configWatcher.close()
+    await close()
+    info('⚙️ Config updated, restarting...')
     await generator(genOpts)
   })
 }
