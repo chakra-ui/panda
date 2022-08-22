c060937a4aa89162eeab587a360ca42345acd17e
Segun Adebayo <joseshegs@gmail.com>
2022-08-22 14:25:50 +0400

    Update generator strategy

--- packages/core/src/generator.ts
+++ packages/core/src/generator.ts
@@ -1,3 +1,4 @@
+import { Stylesheet } from '@css-panda/atomic'
 import { debug } from '@css-panda/logger'
 import { createCollector, createPlugins, transformFileSync } from '@css-panda/parser'
 import { loadConfigFile } from '@css-panda/read-config'
@@ -20,7 +21,40 @@ export async function generator({ outdir, content, cwd }: { outdir: string; cont
   await generateSystem(ctx, { outdir, config: code, clean: true })
 
   /* -----------------------------------------------------------------------------
-   * [codegen] Parse files and extract css
+   * Temp file watcher
+   * -----------------------------------------------------------------------------*/
+
+  const tempPathDir = path.join(outdir, '.temp')
+  fs.ensureDirSync(tempPathDir)
+  const tempWatcher = createWatcher([`${tempPathDir}/*.css`], { cwd })
+
+  const cssPath = path.join(cwd, outdir, 'styles.css')
+
+  function getTempFile(file: string) {
+    return file.replaceAll(path.sep, '__').replace(path.extname(file), '.css')
+  }
+
+  tempWatcher.on('all', async (_event, file) => {
+    debug('🔎 Temp file changed', file)
+
+    ctx.stylesheet.reset()
+    ctx.stylesheet.addImports(['./design-tokens/index.css'])
+
+    const files = fs.readdirSync(tempPathDir)
+
+    await Promise.all(
+      files.map(async (file) => {
+        const css = await fs.readFile(path.join(tempPathDir, file), 'utf8')
+        ctx.stylesheet.append(css)
+      }),
+    )
+
+    const css = ctx.stylesheet.toCss()
+    fs.writeFileSync(cssPath, css)
+  })
+
+  /* -----------------------------------------------------------------------------
+   * Main content watcher
    * -----------------------------------------------------------------------------*/
 
   const watcher = createWatcher(content, {
@@ -28,9 +62,8 @@ export async function generator({ outdir, content, cwd }: { outdir: string; cont
     ignore: ['node_modules', '.git', '__tests__', outdir],
   })
 
-  const cssPath = path.join(cwd, outdir, 'styles.css')
-
   function extract(file: string) {
+    const sheet = new Stylesheet(ctx.context())
     const collector = createCollector()
 
     transformFileSync(file, {
@@ -39,12 +72,18 @@ export async function generator({ outdir, content, cwd }: { outdir: string; cont
     })
 
     collector.css.forEach((result) => {
-      ctx.stylesheet.process(result)
+      sheet.process(result)
     })
 
-    ctx.stylesheet.addImports(['./design-tokens/index.css'])
-    const css = ctx.stylesheet.toCss()
-    fs.writeFileSync(cssPath, css)
+    if (collector.isEmpty()) return
+
+    const filepath = getTempFile(file)
+    const tempPath = path.join(tempPathDir, filepath)
+
+    debug('📝 Writing temp file', tempPath)
+
+    fs.writeFileSync(tempPath, sheet.toCss())
+    sheet.reset()
   }
 
   watcher.on('update', (file) => {
@@ -55,5 +94,10 @@ export async function generator({ outdir, content, cwd }: { outdir: string; cont
   watcher.on('create', (file) => {
     debug(`📝 File detected ====> ${file}`)
     extract(file)
+  })
+
+  watcher.on('delete', (file) => {
+    debug(`📝 File deleted ====> ${file}`)
+    fs.unlinkSync(path.join(tempPathDir, getTempFile(file)))
   })
 }
