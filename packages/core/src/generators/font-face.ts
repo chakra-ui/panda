4509c8794f084826e2a31e1401c488b5e683bd7f
Segun Adebayo <joseshegs@gmail.com>
2022-08-27 19:08:18 +0400

    Add runtime checks for fontFace

--- packages/core/src/generators/font-face.ts
+++ packages/core/src/generators/font-face.ts
@@ -4,6 +4,9 @@ export function generateFontFace() {
   return {
     js: outdent`
     export function fontFace(name, rule){
+        if (typeof name !== "string") {
+          throw new Error("fontFace: name must be a string");
+        }
         return void 0;
     }
     `,
