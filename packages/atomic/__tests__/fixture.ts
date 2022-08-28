3e664ff4e631acd5b4f54ed6dc43d7946d2d8d59
Segun Adebayo <joseshegs@gmail.com>
2022-08-28 13:43:04 +0400

    Fix ts error

--- packages/atomic/__tests__/fixture.ts
+++ packages/atomic/__tests__/fixture.ts
@@ -1,6 +1,7 @@
 import postcss from 'postcss'
 import { GeneratorContext } from '../src/types'
 import { conditions, breakpoints } from '@css-panda/fixture'
+import { CSSCondition } from '../src'
 
 const propMap = {
   display: 'd',
@@ -13,6 +14,7 @@ const propMap = {
 export const createContext = (): GeneratorContext => ({
   root: postcss.root(),
   conditions: conditions,
+  _conditions: new CSSCondition({ conditions, breakpoints }),
   breakpoints,
   transform: (prop, value) => {
     const key = propMap[prop] ?? prop
