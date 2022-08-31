024d2d9cc78c6b8d131f80c70020e9e03bd6564e
Segun Adebayo <joseshegs@gmail.com>
2022-08-31 18:50:33 +0400

    Fix breakpoint details

--- packages/breakpoint-utils/src/index.ts
+++ packages/breakpoint-utils/src/index.ts
@@ -20,32 +20,29 @@ function queries({ name, min, max }: BuildOptions & { name: string }) {
   }
 }
 
-type Detail = {
-  name: string
-  minQuery: string
-  maxQuery: string
-  minMaxQuery: string
+export type BreakpointDetail = ReturnType<typeof queries>
+
+export function sortBreakpoints<T extends string>(breakpoints: Record<T, string>): [string, BreakpointDetail][] {
+  return Object.entries<any>(breakpoints)
+    .sort(([a], [b]) => {
+      return parseInt(a, 10) > parseInt(b, 10) ? -1 : 1
+    })
+    .map(([name, min], index, entries) => {
+      let max: string | null = null
+
+      if (index <= entries.length - 1) {
+        max = entries[index + 1]?.[1]
+      }
+
+      if (max != null) {
+        const computedMax = parseFloat(toPx(max) ?? '') - 0.05
+        max = `${computedMax}px`
+      }
+
+      return [name, queries({ name, min, max })]
+    })
 }
 
 export function getBreakpointDetails<T extends string>(breakpoints: Record<T, string>) {
-  return Object.fromEntries(
-    Object.entries<string>(breakpoints)
-      .sort(([a], [b]) => {
-        return parseInt(a, 10) > parseInt(b, 10) ? 1 : -1
-      })
-      .map(([name, min], index, entries) => {
-        let max: string | null = null
-
-        if (index <= entries.length - 1) {
-          max = entries[index + 1]?.[1]
-        }
-
-        if (max != null) {
-          const _max = parseFloat(toPx(max) ?? '') - 0.05
-          max = `${_max}px`
-        }
-
-        return [name, queries({ name, min, max })]
-      }),
-  ) as Record<T, Detail>
+  return Object.fromEntries(sortBreakpoints(breakpoints)) as Record<T, BreakpointDetail>
 }
