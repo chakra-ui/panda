327aa394b4ebcf903aaa481e476c47399bc9b9ae
Segun Adebayo <joseshegs@gmail.com>
2022-09-02 13:17:23 +0400

    Update classname snapshot

--- packages/atomic/__tests__/classname.test.ts
+++ packages/atomic/__tests__/classname.test.ts
@@ -10,35 +10,18 @@ describe('generate classnames', () => {
         color: { light: 'red', dark: 'green' },
         opacity: { dark: 'slate400' },
       }),
-    ).toMatchInlineSnapshot(`
-      Set {
-        "light:color-red",
-        "dark:color-green",
-        "dark:opacity-slate400",
-      }
-    `)
+    ).toMatchInlineSnapshot('"light:color-red dark:color-green dark:opacity-slate400"')
 
     expect(
       css({
         top: { sm: { rtl: '20px', hover: '50px' }, lg: '120px' },
       }),
-    ).toMatchInlineSnapshot(`
-      Set {
-        "sm:rtl:top-20px",
-        "sm:hover:top-50px",
-        "lg:top-120px",
-      }
-    `)
+    ).toMatchInlineSnapshot('"sm:rtl:top-20px sm:hover:top-50px lg:top-120px"')
 
     expect(
       css({
         left: { _: '20px', md: '40px' },
       }),
-    ).toMatchInlineSnapshot(`
-      Set {
-        "left-20px",
-        "md:left-40px",
-      }
-    `)
+    ).toMatchInlineSnapshot('"left-20px md:left-40px"')
   })
 })
