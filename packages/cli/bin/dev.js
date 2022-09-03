971f5eb604588f48fa500fc0d40227170b515cfc
Segun Adebayo <joseshegs@gmail.com>
2022-09-03 13:26:40 +0400

    Scaffold basic cli package

---
+++ packages/cli/bin/dev.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const { register } = require('esbuild-register/dist/node')
+register()
+
+const { main } = require('../src')
+
+main().catch((e) => {
+  console.error(e)
+  process.exit(1)
+})
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,11 +1,30 @@
-import { Command } from 'commander'
+import { cac } from 'cac'
+import path from 'path'
+import fs from 'fs'
 
-const program = new Command()
+export async function main(options = {}) {
+  const cli = cac('panda')
 
-program.name('css panda')
+  cli
+    .command('[...files]', 'Include files', {
+      ignoreOptionDefaultValue: true,
+    })
+    .option('-o, --out-dir <dir>', 'Output directory', { default: '.panda' })
+    .option('--minify', 'Minify generated code')
+    .option('--cwd <cwd>', 'Current working directory')
+    .option('--watch', 'Watch files and rebuild')
+    .option('--exclude <exclude>', 'Define compile-time env variables')
+    .option('--clean', 'Clean output directory')
+    .option('--silent', 'Suppress non-error logs (excluding "onSuccess" process output)')
+    .action(async (files: string[], flags) => {
+      console.log({ options, files, flags })
+    })
 
-program.command('dev').description('Initialize css panda')
+  cli.help()
 
-program.command('build').description('Build css panda')
+  const pkgPath = path.join(__dirname, '../package.json')
+  cli.version(JSON.parse(fs.readFileSync(pkgPath, 'utf8')).version)
 
-program.parse()
+  cli.parse(process.argv, { run: false })
+  await cli.runMatchedCommand()
+}
