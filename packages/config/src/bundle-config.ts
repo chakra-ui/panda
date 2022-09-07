ead6e7ea872945d0a06ad7da1aefa89024b7d56a
Segun Adebayo <joseshegs@gmail.com>
2022-09-07 15:54:56 +0400

    Add external plugin to esbuild

--- packages/config/src/bundle-config.ts
+++ packages/config/src/bundle-config.ts
@@ -1,10 +1,38 @@
-import { build } from 'esbuild'
+import { build, Plugin } from 'esbuild'
 import { TsconfigPathsPlugin } from '@esbuild-plugins/tsconfig-paths'
+import path from 'path'
+import { lookItUpSync } from 'look-it-up'
+import { pathToFileURL } from 'url'
 
 export async function bundleConfigFile(
   fileName: string,
   isESM = false,
 ): Promise<{ code: string; dependencies: string[] }> {
+  //
+  const externalPlugin: Plugin = {
+    name: 'externalize-deps',
+    setup(build) {
+      build.onResolve({ filter: /.*/ }, ({ path: id, importer }) => {
+        if (id[0] !== '.' && !path.isAbsolute(id)) {
+          return { external: true }
+        }
+
+        const idFsPath = path.resolve(path.dirname(importer), id)
+        const idPkgPath = lookItUpSync(idFsPath, `package.json`)
+
+        if (idPkgPath) {
+          const idPkgDir = path.dirname(idPkgPath)
+          if (path.relative(idPkgDir, fileName).startsWith('..')) {
+            return {
+              path: isESM ? pathToFileURL(idFsPath).href : idFsPath,
+              external: true,
+            }
+          }
+        }
+      })
+    },
+  }
+
   const result = await build({
     absWorkingDir: process.cwd(),
     entryPoints: [fileName],
@@ -15,7 +43,7 @@ export async function bundleConfigFile(
     format: isESM ? 'esm' : 'cjs',
     sourcemap: false,
     metafile: true,
-    plugins: [TsconfigPathsPlugin({})],
+    plugins: [TsconfigPathsPlugin({}), externalPlugin],
   })
 
   const { text } = result.outputFiles[0]
