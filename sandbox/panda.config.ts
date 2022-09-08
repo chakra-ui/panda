642c1f83d61891254f0fd8bdeace918534647482
Segun Adebayo <joseshegs@gmail.com>
2022-09-08 22:00:37 +0400

    Update panda config

--- sandbox/panda.config.ts
+++ sandbox/panda.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'css-panda'
 
 export default defineConfig({
   ...config,
-  clean: false,
-  watch: false,
-  hash: false,
   outdir: 'styled-system',
   include: ['*.jsx'],
 })
