20197254e6bfe7778399dddff4e1069cb0f16d63
Segun Adebayo <joseshegs@gmail.com>
2022-09-09 23:27:23 +0400

    Remove comments

--- packages/shared/src/filter-condition.ts
+++ packages/shared/src/filter-condition.ts
@@ -1,7 +1,3 @@
-/* -----------------------------------------------------------------------------
- * Filter default conditions
- * -----------------------------------------------------------------------------*/
-
 export function filterBaseConditions(conditions: string[]) {
   return conditions.slice().filter((v) => !/^(base|_)$/.test(v))
 }
