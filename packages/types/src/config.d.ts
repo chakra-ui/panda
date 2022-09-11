7930df4eb0257780739c436f517e608e1d857149
Segun Adebayo <joseshegs@gmail.com>
2022-09-11 18:54:23 +0400

    Add js doc comments for types

--- packages/types/src/config.d.ts
+++ packages/types/src/config.d.ts
@@ -20,26 +20,90 @@ export type Config<
   Breakpoints extends Dict = Dict,
   Tokens extends PartialTokens = PartialTokens,
 > = {
-  preflight?: boolean
+  /**
+   * Whether to include css reset styles in the generated css.
+   */
+  preflight?: boolean | { scope: string }
+  /**
+   * Whether to minify the generated css.
+   */
   minify?: boolean
+  /**
+   * The current working directory.
+   */
   cwd?: string
+  /**
+   * Whether to hash the generated class names.
+   * This is useful if want to shorten the class names.
+   */
   hash?: boolean
+  /**
+   * Whether to clean the output directory before generating the css.
+   */
   clean?: boolean
+  /**
+   * The output directory.
+   */
   outdir?: string
+  /**
+   * The css variable options for the generated design tokens
+   */
   cssVar?: {
+    /**
+     * The prefix for the css variables.
+     */
     prefix?: string
+    /**
+     * The root selector for the css variables.
+     * @default ':where(:host, :root)'
+     */
     root?: string
   }
+  /**
+   * Files to watch for changes.
+   */
   include?: string[]
+  /**
+   * Files to ignore.
+   */
   exclude?: string[]
+  /**
+   * Whether to watch for changes and regenerate the css.
+   */
   watch?: boolean
+  /**
+   * The css selectors or media queries shortcuts.
+   * @example `{ hover: "&:hover" }`
+   */
   conditions?: TConditions
+  /**
+   * The breakpoints for your project.
+   */
   breakpoints?: Breakpoints
+  /**
+   * The css animation keyframes definitions.
+   */
   keyframes?: Keyframes
+  /**
+   * The design tokens for your project.
+   */
   tokens?: Tokens
+  /**
+   * The semantic design tokens for your project.
+   */
   semanticTokens?: SemanticTokens<Tokens, Conditions, Breakpoints>
+  /**
+   * The css utility definitions.
+   */
   utilities?: Utility<Tokens>[]
+  /**
+   * Multi-variant style definitions for your project.
+   * Useful for defining component styles.
+   */
   recipes?: Recipe[]
+  /**
+   * Common styling or layout patterns for your project.
+   */
   patterns?: Pattern[]
 }
 
