0a290b6bee166732699a60325c8ef16d99fa935e
Segun Adebayo <joseshegs@gmail.com>
2022-09-15 19:49:55 +0400

    Update sandbox

--- sandbox/vite/src/App.jsx
+++ sandbox/vite/src/App.jsx
@@ -1,7 +1,4 @@
 import { css, globalStyle } from '../styled-system/css'
-import { panda } from '../styled-system/jsx'
-import { stack } from '../styled-system/patterns'
-import { buttonStyle, textStyle } from '../styled-system/recipes'
 import '../styled-system/styles.css'
 
 globalStyle({
@@ -12,35 +9,16 @@ globalStyle({
 
 function App() {
   return (
-    <div className={stack({ gap: '10px', align: 'center' })}>
+    <div>
       <p
         className={css({
           color: 'red.300',
           fontSize: 'lg',
           fontFamily: 'sans-serif',
-          hover: {
-            color: 'red.300',
-            background: { _: 'purple', md: 'red' },
-          },
         })}
       >
         Welcome
       </p>
-      <h1 className={textStyle({ size: 'h2' })}>Beesama</h1>
-      <panda.div
-        fontFamily="body"
-        marginTop="64px"
-        paddingX="4"
-        paddingY="6"
-        backgroundColor="red.200"
-        hover={{
-          backgroundColor: 'blue',
-          color: 'red.300',
-        }}
-      >
-        Welcome home
-      </panda.div>
-      <button className={buttonStyle({ size: 'sm', variant: { md: 'outline' } })}>Teting</button>
     </div>
   )
 }
