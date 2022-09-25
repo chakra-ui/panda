ddedd3eec3d8e33ca6bdd0c90a66aaa68a26c12b
Segun Adebayo <joseshegs@gmail.com>
2022-09-26 01:30:58 +0400

    Update postcss

---
+++ packages/cli/postcss.js
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { Builder } = require('@css-panda/node')
+
+const PLUGIN_NAME = 'css:panda'
+
+function postcssPlugin() {
+  const builder = new Builder()
+
+  return {
+    postcssPlugin: PLUGIN_NAME,
+    plugins: [
+      async function (root, result) {
+        await builder.setup()
+        await builder.extract()
+
+        builder.registerDependency((dep) => {
+          result.messages.push({
+            ...dep,
+            plugin: PLUGIN_NAME,
+            parent: result.opts.from,
+          })
+        })
+
+        builder.write(root)
+      },
+    ],
+  }
+}
+
+postcssPlugin.postcss = true
+
+module.exports = postcssPlugin
