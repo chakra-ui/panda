65aa175848c7e19955ffab5768c438237cfa1822
Segun Adebayo <joseshegs@gmail.com>
2022-09-29 19:35:40 +0400

    Add define patterns to types

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,9 +1,13 @@
-import type { Conditions, Dict, RecipeConfig, RecipeVariant, Config } from '@css-panda/types'
+import type { Conditions, Dict, RecipeConfig, RecipeVariant, Config, PatternConfig } from '@css-panda/types'
 
 export function defineConfig<C extends Conditions, B extends Dict, T extends Dict>(config: Config<C, B, T>): any {
   return config
 }
 
 export function defineRecipe<V extends Record<string, RecipeVariant>>(config: RecipeConfig<V>): any {
+  return config
+}
+
+export function definePattern(config: PatternConfig): any {
   return config
 }
