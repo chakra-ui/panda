2ae58593eafba5c3efdb0ee1a02e18f325eb99d7
Segun Adebayo <joseshegs@gmail.com>
2022-10-08 21:37:03 +0400

    Update presets

--- packages/presets/src/typography.ts
+++ packages/presets/src/typography.ts
@@ -1,8 +1,9 @@
-import type { TextStyles } from '@css-panda/types'
+import type { TextStyles, Tokens } from '@css-panda/types'
 
-export const fontSizes = {
-  '2xs': { value: '0.75rem' },
-  xs: { value: '0.875rem' },
+export const fontSizes: Tokens['fontSizes'] = {
+  '2xs': { value: '0.5rem' },
+  xs: { value: '0.75rem' },
+  sm: { value: '0.875rem' },
   md: { value: '1rem' },
   lg: { value: '1.125rem' },
   xl: { value: '1.25rem' },
@@ -16,7 +17,7 @@ export const fontSizes = {
   '9xl': { value: '8rem' },
 }
 
-export const fontWeights = {
+export const fontWeights: Tokens['fontWeights'] = {
   thin: { value: '100' },
   extralight: { value: '200' },
   light: { value: '300' },
@@ -28,7 +29,7 @@ export const fontWeights = {
   black: { value: '900' },
 }
 
-export const letterSpacings = {
+export const letterSpacings: Tokens['letterSpacings'] = {
   tighter: { value: '-0.05em' },
   tight: { value: '-0.025em' },
   normal: { value: '0em' },
@@ -37,7 +38,7 @@ export const letterSpacings = {
   widest: { value: '0.1em' },
 }
 
-export const lineHeights = {
+export const lineHeights: Tokens['lineHeights'] = {
   none: { value: '1' },
   tight: { value: '1.25' },
   snug: { value: '1.375' },
@@ -46,7 +47,7 @@ export const lineHeights = {
   loose: { value: '2' },
 }
 
-export const fonts = {
+export const fonts: Tokens['fonts'] = {
   sans: {
     value: [
       'ui-sans-serif',
@@ -83,17 +84,82 @@ export const fonts = {
 }
 
 export const textStyles: TextStyles = {
-  xs: { value: { fontSize: '0.75rem', lineHeight: '1rem' } },
-  sm: { value: { fontSize: '0.875rem', lineHeight: '1.25rem' } },
-  md: { value: { fontSize: '1rem', lineHeight: '1.5rem' } },
-  lg: { value: { fontSize: '1.125rem', lineHeight: '1.75rem' } },
-  xl: { value: { fontSize: '1.25rem', lineHeight: '1.75rem' } },
-  '2xl': { value: { fontSize: '1.5rem', lineHeight: '2rem' } },
-  '3xl': { value: { fontSize: '1.875rem', lineHeight: '2.25rem' } },
-  '4xl': { value: { fontSize: '2.25rem', lineHeight: '2.5rem' } },
-  '5xl': { value: { fontSize: '3rem', lineHeight: '1' } },
-  '6xl': { value: { fontSize: '3.75rem', lineHeight: '1' } },
-  '7xl': { value: { fontSize: '4.5rem', lineHeight: '1' } },
-  '8xl': { value: { fontSize: '6rem', lineHeight: '1' } },
-  '9xl': { value: { fontSize: '8rem', lineHeight: '1' } },
+  xs: {
+    value: {
+      fontSize: '0.75rem',
+      lineHeight: '1rem',
+    },
+  },
+  sm: {
+    value: {
+      fontSize: '0.875rem',
+      lineHeight: '1.25rem',
+    },
+  },
+  md: {
+    value: {
+      fontSize: '1rem',
+      lineHeight: '1.5rem',
+    },
+  },
+  lg: {
+    value: {
+      fontSize: '1.125rem',
+      lineHeight: '1.75rem',
+    },
+  },
+  xl: {
+    value: {
+      fontSize: '1.25rem',
+      lineHeight: '1.75rem',
+    },
+  },
+  '2xl': {
+    value: {
+      fontSize: '1.5rem',
+      lineHeight: '2rem',
+    },
+  },
+  '3xl': {
+    value: {
+      fontSize: '1.875rem',
+      lineHeight: '2.25rem',
+    },
+  },
+  '4xl': {
+    value: {
+      fontSize: '2.25rem',
+      lineHeight: '2.5rem',
+    },
+  },
+  '5xl': {
+    value: {
+      fontSize: '3rem',
+      lineHeight: '1',
+    },
+  },
+  '6xl': {
+    value: {
+      fontSize: '3.75rem',
+      lineHeight: '1',
+    },
+  },
+  '7xl': {
+    value: {
+      fontSize: '4.5rem',
+      lineHeight: '1',
+    },
+  },
+  '8xl': {
+    value: {
+      fontSize: '6rem',
+      lineHeight: '1',
+    },
+  },
+  '9xl': {
+    value: {
+      fontSize: '8rem',
+      lineHeight: '1',
+    },
+  },
 }
