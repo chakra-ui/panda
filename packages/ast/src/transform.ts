1035e83871e95a29010438d9e962ede7f14ee304
Segun Adebayo <joseshegs@gmail.com>
2022-10-14 14:42:04 +0400

    Add support for decorators in swc

--- packages/ast/src/transform.ts
+++ packages/ast/src/transform.ts
@@ -1,21 +1,22 @@
 import * as swc from '@swc/core'
 
-export async function parseFile(file: string, plugins: swc.Plugin[]) {
-  const ast = await swc.parseFile(file, { tsx: true, syntax: 'typescript' })
-  plugins.forEach((plugin) => {
-    plugin(ast)
-  })
-}
-export async function parse(code: string, plugins: swc.Plugin[]) {
-  const ast = await swc.parse(code, { tsx: true, syntax: 'typescript' })
-  plugins.forEach((plugin) => {
-    plugin(ast)
-  })
+const options: swc.ParseOptions = {
+  tsx: true,
+  syntax: 'typescript',
+  decorators: true,
+  dynamicImport: true,
 }
 
-export async function parseSync(code: string, plugins: swc.Plugin[]) {
-  const ast = swc.parseSync(code, { tsx: true, syntax: 'typescript' })
-  plugins.forEach((plugin) => {
-    plugin(ast)
-  })
+export async function parseFile(file: string, plugins: swc.Plugin[]) {
+  const ast = await swc.parseFile(file, options)
+  plugins.forEach((plugin) => plugin(ast))
+}
+export async function parse(code: string, plugins: swc.Plugin[]) {
+  const ast = await swc.parse(code, options)
+  plugins.forEach((plugin) => plugin(ast))
+}
+
+export function parseSync(code: string, plugins: swc.Plugin[]) {
+  const ast = swc.parseSync(code, options)
+  plugins.forEach((plugin) => plugin(ast))
 }
