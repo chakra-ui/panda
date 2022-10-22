432533a3c61fe4f71aa8f6513cfa888c38d5ed68
Segun Adebayo <joseshegs@gmail.com>
2022-10-22 13:56:58 +0400

    Restructure token tests

---
+++ packages/dictionary/__tests__/transform-gradient.test.ts
@@ -0,0 +1,77 @@
+import { expect, test } from 'vitest'
+import { TokenDictionary } from '../src/dictionary'
+import { transformGradient } from '../src/transform'
+
+test('transform / gradient', () => {
+  const dictionary = new TokenDictionary({
+    tokens: {
+      colors: {
+        pink: { value: '#ff00ff' },
+      },
+      gradients: {
+        primary: {
+          value: {
+            type: 'linear',
+            placement: 'to top',
+            stops: [
+              { color: '#ff0000', position: 0 },
+              { color: '{colors.pink}', position: 100 },
+            ],
+          },
+        },
+      },
+    },
+  })
+
+  dictionary.registerTransform(transformGradient)
+
+  dictionary.build()
+
+  expect(dictionary.allTokens).toMatchInlineSnapshot(`
+      [
+        Token {
+          "extensions": {
+            "category": "colors",
+            "condition": "base",
+            "prop": "pink",
+          },
+          "name": "colors.pink",
+          "originalValue": "#ff00ff",
+          "path": [
+            "colors",
+            "pink",
+          ],
+          "type": "color",
+          "value": "#ff00ff",
+        },
+        Token {
+          "extensions": {
+            "category": "gradients",
+            "condition": "base",
+            "prop": "primary",
+          },
+          "name": "gradients.primary",
+          "originalValue": {
+            "placement": "to top",
+            "stops": [
+              {
+                "color": "#ff0000",
+                "position": 0,
+              },
+              {
+                "color": "{colors.pink}",
+                "position": 100,
+              },
+            ],
+            "type": "linear",
+          },
+          "path": [
+            "gradients",
+            "primary",
+          ],
+          "type": "gradient",
+          "value": "linear-gradient(to top, #ff0000 0px, #ff00ff 100px)",
+        },
+      ]
+    `)
+})
--- packages/dictionary/__tests__/transform-shadow.test.ts
+++ packages/dictionary/__tests__/transform-shadow.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest'
 import { TokenDictionary } from '../src/dictionary'
-import { transformGradient, transformShadow } from '../src/transform'
+import { transformShadow } from '../src/transform'
 
 test('transform / shadow', () => {
   const dictionary = new TokenDictionary({
@@ -71,80 +71,6 @@ test('transform / shadow', () => {
         ],
         "type": "shadow",
         "value": "0 4px 6px -1px rgba(0, 0, 0, 0.1)",
-      },
-    ]
-  `)
-})
-
-test('transform / gradient', () => {
-  const dictionary = new TokenDictionary({
-    tokens: {
-      colors: {
-        pink: { value: '#ff00ff' },
-      },
-      gradients: {
-        primary: {
-          value: {
-            type: 'linear',
-            placement: 'to top',
-            stops: [
-              { color: '#ff0000', position: 0 },
-              { color: '{colors.pink}', position: 100 },
-            ],
-          },
-        },
-      },
-    },
-  })
-
-  dictionary.registerTransform(transformGradient)
-
-  dictionary.build()
-
-  expect(dictionary.allTokens).toMatchInlineSnapshot(`
-    [
-      Token {
-        "extensions": {
-          "category": "colors",
-          "condition": "base",
-          "prop": "pink",
-        },
-        "name": "colors.pink",
-        "originalValue": "#ff00ff",
-        "path": [
-          "colors",
-          "pink",
-        ],
-        "type": "color",
-        "value": "#ff00ff",
-      },
-      Token {
-        "extensions": {
-          "category": "gradients",
-          "condition": "base",
-          "prop": "primary",
-        },
-        "name": "gradients.primary",
-        "originalValue": {
-          "placement": "to top",
-          "stops": [
-            {
-              "color": "#ff0000",
-              "position": 0,
-            },
-            {
-              "color": "{colors.pink}",
-              "position": 100,
-            },
-          ],
-          "type": "linear",
-        },
-        "path": [
-          "gradients",
-          "primary",
-        ],
-        "type": "gradient",
-        "value": "linear-gradient(to top, #ff0000 0px, #ff00ff 100px)",
       },
     ]
   `)
