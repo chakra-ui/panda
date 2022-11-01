87b4df898796929145cd9d1f8515a851a46fe59a
Segun Adebayo <joseshegs@gmail.com>
2022-11-01 16:51:35 +0400

    Add custom postcss plugin

--- packages/core/src/optimize.ts
+++ packages/core/src/optimize.ts
@@ -1,26 +1,27 @@
-import discardDuplicates from 'postcss-discard-duplicates'
-// import normalizeWhiteSpace from 'postcss-normalize-whitespace'
-// import prettify from 'postcss-prettify'
-import postcss from 'postcss'
-// import sortMq from 'postcss-sort-media-queries'
+import normalizeWhiteSpace from 'postcss-normalize-whitespace'
+import postcss, { AcceptedPlugin } from 'postcss'
 import nested from 'postcss-nested'
 // import prefixer from 'autoprefixer'
 import { mergeLayers } from './plugins/merge-layers'
 import { sortMediaQueries } from './plugins/sort-mq'
+import dudupe from './plugins/dudupe'
 
 export function optimizeCss(code: string, { minify = false }: { minify?: boolean } = {}) {
-  const { css } = postcss([
-    discardDuplicates(),
-    mergeLayers(),
-    sortMediaQueries(),
-    // minify ? normalizeWhiteSpace() : prettify,
-    nested(),
-    // prefixer(),
-  ]).process(code)
+  const { css } = postcss(
+    [
+      mergeLayers(),
+      sortMediaQueries(),
+      dudupe(),
+      nested(),
+      minify && normalizeWhiteSpace(),
+      // prefixer(),
+    ].filter(Boolean) as AcceptedPlugin[],
+  ).process(code)
+
   return css
 }
 
 export function discardDuplicate(code: string) {
-  const { css } = postcss([discardDuplicates()]).process(code)
+  const { css } = postcss([dudupe()]).process(code)
   return css
 }
