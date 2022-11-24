459a95872263a69732c1fc60a1ba58a8b47eae35
anubra266 <anubra266@gmail.com>
2022-11-24 14:41:32 +0100

    chore: add home page to cli docs

--- packages/cli/app/src/pages/index.tsx
+++ packages/cli/app/src/pages/index.tsx
@@ -1,11 +1,61 @@
-import { config } from 'virtual:panda'
-import { TokenDictionary } from '@pandacss/token-dictionary'
+import { navItems } from '../utils/constants'
+import { NavLink } from 'react-router-dom'
 
 function Index() {
-  const tokenDictionary = new TokenDictionary(config)
-  const tokens = Object.fromEntries(tokenDictionary.categoryMap)
-
-  return <div>Home</div>
+  return (
+    <div className="home">
+      <span className="title">🐼 Panda</span>
+      <div className="banner">
+        <span className="intro">Design System</span>
+        <span>Build great products, faster.</span>
+        <NavLink to="/colors" className="cta">
+          Explore
+        </NavLink>
+      </div>
+      <div className="content">
+        <div>
+          <span>TOKENS</span>
+          <ul>
+            {navItems
+              .filter((k) => k.type === 'token')
+              .map((themeKey) => (
+                <NavLink key={themeKey.id} to={`/${themeKey.id}`}>
+                  {({ isActive }) => (
+                    <li className="item" data-active={isActive ? '' : undefined}>
+                      <span className="icon">
+                        <themeKey.icon />
+                      </span>
+                      <span className="title"> {themeKey.label} </span>
+                      <span className="description"> {themeKey.description} </span>
+                    </li>
+                  )}
+                </NavLink>
+              ))}
+          </ul>
+        </div>
+        <div>
+          <span>PLAYGROUND</span>
+          <ul>
+            {navItems
+              .filter((k) => k.type === 'playground')
+              .map((themeKey) => (
+                <NavLink key={themeKey.id} to={`/${themeKey.id}`}>
+                  {({ isActive }) => (
+                    <li className="item" data-active={isActive ? '' : undefined}>
+                      <span className="icon">
+                        <themeKey.icon />
+                      </span>
+                      <span className="title"> {themeKey.label} </span>
+                      <span className="description"> {themeKey.description} </span>
+                    </li>
+                  )}
+                </NavLink>
+              ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Index
