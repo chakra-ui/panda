8b2e38d4a6c6570875f8c5c85cadd2ea8966be8f
anubra266 <anubra266@gmail.com>
2022-11-30 13:40:59 +0100

    chore: fix types

--- packages/cli/app/src/components/token-content.tsx
+++ packages/cli/app/src/components/token-content.tsx
@@ -1,6 +1,6 @@
 import { panda } from 'design-system/jsx'
-import React from 'react'
+import type { PropsWithChildren } from 'react'
 
-export function TokenContent(props: React.ReactNode) {
+export function TokenContent(props: PropsWithChildren) {
   return <panda.div display="flex" flexDir="column" gap="5" {...props} />
 }
--- packages/cli/app/src/components/token-group.tsx
+++ packages/cli/app/src/components/token-group.tsx
@@ -1,6 +1,6 @@
 import { panda } from 'design-system/jsx'
-import React from 'react'
+import type { PropsWithChildren } from 'react'
 
-export function TokenGroup(props: React.ReactNode) {
+export function TokenGroup(props: PropsWithChildren) {
   return <panda.div display="flex" flexDir="column" gap="3" width="full" marginTop="5" {...props} />
 }
