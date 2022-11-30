930c195e7273d888bedbe7ea97b3db89a0a4fbe5
anubra266 <anubra266@gmail.com>
2022-11-30 14:08:08 +0100

    reactor: typings

--- packages/cli/app/src/components/token-content.tsx
+++ packages/cli/app/src/components/token-content.tsx
@@ -1,6 +1,6 @@
 import { panda } from 'design-system/jsx'
 import type { PropsWithChildren } from 'react'
 
-export function TokenContent(props: PropsWithChildren) {
+export function TokenContent(props: PropsWithChildren<JSXStyleProperties>) {
   return <panda.div display="flex" flexDir="column" gap="5" {...props} />
 }
--- packages/cli/app/src/components/token-group.tsx
+++ packages/cli/app/src/components/token-group.tsx
@@ -1,6 +1,7 @@
 import { panda } from 'design-system/jsx'
 import type { PropsWithChildren } from 'react'
+import { JSXStyleProperties } from 'design-system/types'
 
-export function TokenGroup(props: PropsWithChildren) {
+export function TokenGroup(props: PropsWithChildren<JSXStyleProperties>) {
   return <panda.div display="flex" flexDir="column" gap="3" width="full" marginTop="5" {...props} />
 }
