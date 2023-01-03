5132d76e7cf7477cff559b3a431f85f73f7022fe
Segun Adebayo <joseshegs@gmail.com>
2023-01-03 14:15:08 +0400

    chore: omit artifact from prettier

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,4 +1,13 @@
-import type { Conditions, Dict, RecipeConfig, RecipeVariant, Config, PatternConfig, Parts } from '@pandacss/types'
+import type {
+  Conditions,
+  Dict,
+  RecipeConfig,
+  RecipeVariant,
+  Config,
+  PatternConfig,
+  Parts,
+  StyleObject,
+} from '@pandacss/types'
 
 export function defineConfig<C extends Conditions, B extends Dict, T extends Dict>(config: Config<C, B, T>): any {
   return config
@@ -13,10 +22,7 @@ export function definePattern(config: PatternConfig): any {
 }
 
 export function defineParts<T extends Parts>(parts: T) {
-  return function (config: Partial<Record<keyof T, any>>) {
-    return Object.fromEntries(Object.entries(config).map(([key, value]) => [parts[key].selector, value]))
+  return function (config: Partial<Record<keyof T, StyleObject>>): Partial<Record<keyof T, StyleObject>> {
+    return Object.fromEntries(Object.entries(config).map(([key, value]) => [parts[key].selector, value])) as any
   }
 }
-
-// TODO: defineToken, defineTokenGroup
-// TODO: defineSemanticToken, defineSemanticTokenGroup
