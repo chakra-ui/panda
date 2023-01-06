9a53cbfbbbe8e12c950bfb174f9f789a4db8d68a
Segun Adebayo <joseshegs@gmail.com>
2023-01-06 20:57:44 +0400

    feat: add split object

--- packages/shared/src/shared.ts
+++ packages/shared/src/shared.ts
@@ -2,10 +2,10 @@
  * This code is shared between the runtime and cli
  */
 
-export * from './condition'
 export * from './classname'
+export * from './compact'
+export * from './condition'
+export { withoutSpace } from './css-important'
 export * from './hash'
 export * from './walk-object'
 export * from './walk-styles'
-export * from './compact'
-export { withoutSpace } from './css-important'
