b11ba2d91246af364612a19fb649526e5485710e
Segun Adebayo <joseshegs@gmail.com>
2023-01-06 22:28:49 +0400

    chore: add preact sandbox

---
+++ sandbox/preact-ts/panda.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'css-panda'
+
+export default defineConfig({
+  preflight: true,
+  include: ['./src/**/*.{tsx,jsx}', './pages/**/*.{jsx,tsx}'],
+  exclude: [],
+  outdir: 'design-system',
+  jsxFramework: 'preact',
+})
---
+++ sandbox/preact-ts/src/app.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <>
+      <h1>Vite + Preact</h1>
+    </>
+  )
+}
