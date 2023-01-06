f610bdf29cfea2b9d1956f40fc236036160b47a2
Segun Adebayo <joseshegs@gmail.com>
2023-01-06 12:21:31 +0400

    fix: issue with compact

--- packages/shared/src/compact.ts
+++ packages/shared/src/compact.ts
@@ -1,3 +1,4 @@
 export function compact<T extends Record<string, any>>(value: T) {
+  if (value == null) return {}
   return Object.fromEntries(Object.entries(value).filter(([_, value]) => value !== undefined)) as T
 }
