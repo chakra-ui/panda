892c3ae683da9733b1532f8c8551680f658eecce
Segun Adebayo <joseshegs@gmail.com>
2023-01-18 16:49:07 +0400

    refactor: use shorter hash

--- packages/shared/src/hash.ts
+++ packages/shared/src/hash.ts
@@ -1,20 +1,6 @@
-function toChar(code: number) {
-  return String.fromCharCode(code + (code > 25 ? 39 : 97))
-}
-
-function toName(code: number) {
-  let name = ''
-  let x: number
-  for (x = Math.abs(code); x > 52; x = (x / 52) | 0) name = toChar(x % 52) + name
-  return toChar(x % 52) + name
-}
-
-function toPhash(h: number, x: string) {
-  let i = x.length
-  while (i) h = (h * 33) ^ x.charCodeAt(--i)
-  return h
-}
-
-export function toHash(value: string) {
-  return toName(toPhash(5381, value) >>> 0)
+export function toHash(str: string) {
+  let value = 5381
+  let len = str.length
+  while (len--) value = (value * 33) ^ str.charCodeAt(len)
+  return (value >>> 0).toString(36)
 }
