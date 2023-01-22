4dda5860858dd32a3643953628f78c41d547f0f6
anubra266 <anubra266@gmail.com>
2023-01-22 15:22:05 +0100

    feat(sandbox): add remix example

---
+++ sandbox/remix/app/components/Badge.tsx
@@ -0,0 +1,34 @@
+import { panda } from 'design-system/jsx'
+import { cva } from 'design-system/css'
+
+export const badge = cva({
+  base: {
+    fontWeight: 'medium',
+    letterSpacing: 'wide',
+    flexGrow: '0',
+    px: '3',
+    alignSelf: 'flex-start',
+    borderRadius: 'md',
+  },
+  variants: {
+    status: {
+      default: {
+        color: 'white',
+        bg: 'gray.500',
+      },
+      success: {
+        color: 'white',
+        bg: 'green.500',
+      },
+      warning: {
+        color: 'white',
+        bg: 'yellow.500',
+      },
+    },
+  },
+  defaultVariants: {
+    status: 'default',
+  },
+})
+
+export const Badge = panda('span', badge)
---
+++ sandbox/remix/panda.config.ts
@@ -0,0 +1,62 @@
+import { defineConfig } from 'css-panda'
+
+export default defineConfig({
+  outExtension: 'js',
+  studio: {
+    title: 'Chakra UI',
+    logo: 'https://place-hold.it/36x24',
+  },
+  preflight: true,
+  include: ['./app/routes/**/*.{tsx,jsx}', './app/components/**/*.{tsx,jsx}'],
+  exclude: [],
+  outdir: 'design-system',
+  theme: {
+    semanticTokens: {
+      colors: {
+        text: { value: { base: '{colors.gray.600}', _osDark: '{colors.gray.400}' } },
+      },
+    },
+    recipes: {
+      button: {
+        name: 'button',
+        description: 'A button styles',
+        base: {
+          fontSize: 'lg',
+        },
+        variants: {
+          size: {
+            sm: {
+              padding: '2',
+              borderRadius: 'sm',
+            },
+            md: {
+              padding: '4',
+              borderRadius: 'md',
+            },
+          },
+          variant: {
+            primary: {
+              color: 'white',
+              backgroundColor: 'blue.500',
+            },
+            danger: {
+              color: 'white',
+              backgroundColor: 'red.500',
+            },
+          },
+        },
+      },
+    },
+  },
+  jsxFramework: 'react',
+  globalCss: {
+    '*': {
+      fontFamily: 'Inter',
+      margin: '0',
+    },
+    a: {
+      color: 'inherit',
+      textDecoration: 'none',
+    },
+  },
+})
