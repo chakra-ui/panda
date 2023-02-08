bdeccff78d9a8411bd28b35a4e5168d7ca2e6e30
Tim Kolberger <tim@kolberger.eu>
2023-02-08 19:38:02 +0100

    feat(playground): include user theme

--- playground/postcss.config.js
+++ playground/postcss.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  plugins: ['css-panda/postcss'],
+  plugins: { 'css-panda/postcss': {} },
 }
--- playground/src/pages/api/share.ts
+++ playground/src/pages/api/share.ts
@@ -6,7 +6,7 @@ import { prisma } from '../../client/prisma'
 
 const schema = z.object({
   code: z.string(),
-  config: z.string(),
+  theme: z.string(),
   view: z.enum(['code', 'config']).optional(),
 })
 
@@ -14,9 +14,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) =>
   match(req)
     .with({ method: 'POST' }, async () => {
       try {
-        const { code, config } = schema.parse(req.body)
+        const { code, theme } = schema.parse(req.body)
         const id = nanoid(10)
-        await prisma.session.create({ data: { id, code, config } })
+        await prisma.session.create({ data: { id, code, theme } })
         return res.status(200).json({ id })
       } catch (e) {
         console.log(e)
