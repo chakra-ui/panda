fde2ca8c64c8f98c9524dd1a1e7e3892dfd250b5
Abraham <anubra266@gmail.com>
2023-02-15 16:31:24 +0100

    chore: use postcss in remix sandbox (#247)

--- sandbox/remix/remix.config.js
+++ sandbox/remix/remix.config.js
@@ -1,13 +1,16 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "vercel",
+  serverBuildTarget: 'vercel',
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  ignoredRouteFiles: ["**/.*"],
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+  ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
-};
+  future: {
+    unstable_postcss: true,
+  },
+}
