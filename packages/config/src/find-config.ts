2c54f50992b8b73b536c01cbdb6b3d0bb6d0ece7
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-03-02 11:44:56 +0100

    feat: expose findConfigFile for the CLI

--- packages/config/src/find-config.ts
+++ packages/config/src/find-config.ts
@@ -2,11 +2,13 @@ import findUp from 'escalade/sync'
 import { resolve } from 'path'
 
 const configs = ['.ts', '.js', '.mjs', '.cjs']
+const pandaConfigRegex = new RegExp(`panda.config(${configs.join('|')})$`)
+
+const isPandaConfig = (file: string) => pandaConfigRegex.test(file)
 
 export function findConfigFile({ cwd, file }: { cwd: string; file?: string }) {
   if (file) return resolve(cwd, file)
   return findUp(cwd, (_dir, paths) => {
-    const regex = new RegExp(`panda.config(${configs.join('|')})$`)
-    return paths.find((p) => regex.test(p))
+    return paths.find(isPandaConfig)
   })
 }
