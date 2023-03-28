de83b951338b273dc53f4b355c706211fc86473c
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-03-28 11:56:01 +0200

    feat: add 2 latests versions of @box-extractor/core to the bench

--- packages/extractor/__tests__/npm-bench/tests/extract.bench.ts
+++ packages/extractor/__tests__/npm-bench/tests/extract.bench.ts
@@ -1,11 +1,13 @@
 import { bench, describe } from 'vitest'
 
-import { extract as extract080 } from '@box-extractor/core-0-8-0'
 import { extract as extract075 } from '@box-extractor/core-0-7-5'
+import { extract as extract080 } from '@box-extractor/core-0-8-0'
+import { extract as extract084 } from '@box-extractor/core-0-8-4'
+import { extract as extract090 } from '@box-extractor/core-0-9-0'
 import { Project, ts } from 'ts-morph'
 import { extract } from '../../../src/extract'
 // @ts-expect-error
-import { default as ExtractSample } from '../ExtractSample?raw'
+import { default as ExtractSample } from '../../ExtractSample?raw'
 import { createProject } from '../../createProject'
 
 const project: Project = createProject()
@@ -20,13 +22,15 @@ const config = {
   },
 }
 
+const iterations = 3000
+
 describe('extract across versions', () => {
   bench(
     '0.7.5',
     () => {
       extract075({ ast: sourceFile, components: config })
     },
-    { iterations: 3000 },
+    { iterations },
   )
 
   bench(
@@ -40,7 +44,35 @@ describe('extract across versions', () => {
         },
       })
     },
-    { iterations: 3000 },
+    { iterations },
+  )
+
+  bench(
+    '0.8.4',
+    () => {
+      extract084({
+        ast: sourceFile,
+        components: {
+          matchTag: ({ tagName }) => Boolean(config[tagName]),
+          matchProp: ({ tagName, propName }) => config[tagName].map.get(propName),
+        },
+      })
+    },
+    { iterations },
+  )
+
+  bench(
+    '0.9.0',
+    () => {
+      extract090({
+        ast: sourceFile,
+        components: {
+          matchTag: ({ tagName }) => Boolean(config[tagName]),
+          matchProp: ({ tagName, propName }) => config[tagName].map.get(propName),
+        },
+      })
+    },
+    { iterations },
   )
 
   bench(
@@ -54,6 +86,6 @@ describe('extract across versions', () => {
         },
       })
     },
-    { iterations: 3000 },
+    { iterations },
   )
 })
