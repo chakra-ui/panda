b8fa448a7e1e8479ca3f24390082839a90986990
Alexandre Stahmer <47224540+astahmer@users.noreply.github.com>
2023-04-11 22:05:19 +0200

    refactor(extract): spread to work as expected, only keep the last one (#441)

--- packages/extractor/src/extract.ts
+++ packages/extractor/src/extract.ts
@@ -20,20 +20,27 @@ import type {
 import { getComponentName } from './utils'
 
 type JsxElement = JsxOpeningElement | JsxSelfClosingElement
-type ComponentMap = Map<JsxElement, { name: string; props: MapTypeValue; spreads: BoxNodeMap[] }>
+type ComponentMap = Map<JsxElement, { name: string; props: MapTypeValue; conditionals: BoxNodeConditional[] }>
 
-const isJsxElement = Bool.or(Node.isJsxOpeningElement, Node.isJsxSelfClosingElement)
 const isImportOrExport = Bool.or(Node.isImportDeclaration, Node.isExportDeclaration)
 
 export const extract = ({ ast, ...ctx }: ExtractOptions) => {
   const { components, functions } = ctx
 
-  // contains all the extracted nodes from this ast parsing
-  const result: ExtractResultByName = new Map()
-  const componentMap: ComponentMap = new Map()
+  /** contains all the extracted nodes from this ast parsing */
+  const byName: ExtractResultByName = new Map()
+  /**
+   * associate a component node with its props and (spread) conditionals
+   * since js es6 map preserve insertion order, we can use it to keep the order of the props
+   * so we can keep the last one
+   * ex: <ColorBox padding="4" {...{ color: "blue.100" }} color="red" margin={2} />
+   * => color: "red"
+   */
+  const componentByNode: ComponentMap = new Map()
 
-  const visitedComponentFromSpread = new WeakSet<Node>()
-  const spreadByComponent = new WeakMap<JsxElement, { props: MapTypeValue; conditionals: BoxNodeConditional[] }>()
+  // keep track of the current component node
+  // so we don't have to traverse the tree upwards again
+  let componentNode: JsxElement | undefined
 
   ast.forEachDescendant((node, traversal) => {
     // quick win
@@ -42,160 +49,122 @@ export const extract = ({ ast, ...ctx }: ExtractOptions) => {
       return
     }
 
-    if (components && Node.isJsxSpreadAttribute(node)) {
-      // <ColorBox {...{ color: "facebook.100" }}>spread</ColorBox>
-      //           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-      const componentNode = node.getFirstAncestor(isJsxElement)
-      if (!componentNode) return
-
-      // skip re-extracting nested spread attribute, already extracted from the parent spread
-      // <ColorBox {...{ color: "facebook.100", ...{ backgroundColor: "facebook.200" } }}>spread</ColorBox>
-      //                                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-      if (visitedComponentFromSpread.has(componentNode)) {
-        traversal.skip()
-        return
+    if (components) {
+      if (Node.isJsxOpeningElement(node) || Node.isJsxSelfClosingElement(node)) {
+        componentNode = node
       }
 
-      visitedComponentFromSpread.add(componentNode)
+      if (Node.isJsxSpreadAttribute(node)) {
+        // <ColorBox {...{ color: "facebook.100" }}>spread</ColorBox>
+        //           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-      const componentName = getComponentName(componentNode)
-      const isFactory = componentName.includes('.')
+        if (!componentNode) return
 
-      if (!components.matchTag({ tagNode: componentNode, tagName: componentName, isFactory })) {
-        return
-      }
+        const componentName = getComponentName(componentNode)
+        const isFactory = componentName.includes('.')
 
-      if (!result.has(componentName)) {
-        result.set(componentName, { kind: 'component', nodesByProp: new Map(), queryList: [] })
-      }
-
-      const boxByProp = result.get(componentName)!.nodesByProp
-
-      if (!componentMap.has(componentNode)) {
-        componentMap.set(componentNode, { name: componentName, props: new Map(), spreads: [] })
-      }
-
-      const matchProp = ({ propName, propNode }: MatchPropArgs) =>
-        components.matchProp({ tagNode: componentNode, tagName: componentName, propName, propNode })
-
-      const spreadNode = extractJsxSpreadAttributeValues(node, ctx, cast(matchProp))
-      if (!spreadNode) return
-
-      // <ColorBox padding="4" {...{ color: "facebook.100" }} margin={2} />
-      // the parent ref contains the props that were already extracted from the jsx attributes (not spread)
-      // so we can merge the spread props with those extracted props
-      const parentRef = componentMap.get(componentNode)!
-
-      if (!spreadByComponent.has(componentNode)) {
-        spreadByComponent.set(componentNode, { props: new Map(), conditionals: [] })
-      }
-      const spread = spreadByComponent.get(componentNode)!
-
-      // TODO keep track of the JsxAttributes[element] index (including spread) to keep track of the order before & after the spread
-      // so we know which props can be overriden and avoid generating styles for those that were overriden
-
-      const processObjectLike = (objLike: BoxNodeMap | BoxNodeObject, isRootConditional?: boolean) => {
-        const mapValue = objectLikeToMap(objLike, node)
-        const boxNode = box.map(mapValue, node, [componentNode])
-
-        if (box.isMap(objLike) && objLike.spreadConditions?.length) {
-          boxNode.spreadConditions = objLike.spreadConditions
+        if (!components.matchTag({ tagNode: componentNode, tagName: componentName, isFactory })) {
+          return
         }
 
-        const entries = mergeSpreads({
-          map: mapValue,
-          // if the boxNode is an object
-          // that means it was evaluated so we need to filter its props
-          // otherwise, it was already filtered in extractJsxSpreadAttributeValues
-          matchProp: box.isObject(objLike) ? (matchProp as any) : undefined,
-        })
+        if (!byName.has(componentName)) {
+          byName.set(componentName, { kind: 'component', nodesByProp: new Map(), queryList: [] })
+        }
 
-        // <ColorBox {...(someCondition && { color: "facebook.100" })} />
-        if (isRootConditional) {
-          const boxMap = box.map(spread.props, node, boxNode.getStack())
-          if (box.isMap(boxNode) && boxNode.spreadConditions?.length) {
-            boxMap.spreadConditions = boxNode.spreadConditions
+        const boxByProp = byName.get(componentName)!.nodesByProp
+
+        if (!componentByNode.has(componentNode)) {
+          componentByNode.set(componentNode, { name: componentName, props: new Map(), conditionals: [] })
+        }
+
+        const matchProp = ({ propName, propNode }: MatchPropArgs) =>
+          components.matchProp({ tagNode: componentNode!, tagName: componentName, propName, propNode })
+
+        const spreadNode = extractJsxSpreadAttributeValues(node, ctx, cast(matchProp))
+        if (!spreadNode) return
+
+        // <ColorBox padding="4" {...{ color: "facebook.100" }} margin={2} />
+        // the parent ref contains the props that were already extracted from the jsx attributes (not spread)
+        // so we can merge the spread props with those extracted props
+        const component = componentByNode.get(componentNode)!
+
+        const processObjectLike = (objLike: BoxNodeMap | BoxNodeObject) => {
+          const mapValue = objectLikeToMap(objLike, node)
+          const isMap = box.isMap(objLike)
+          const boxNode = box.map(mapValue, node, [componentNode!])
+
+          if (isMap && objLike.spreadConditions?.length) {
+            boxNode.spreadConditions = objLike.spreadConditions
           }
-          parentRef.spreads.push(boxMap)
 
-          return boxNode
-        }
-
-        entries.forEach(([propName, propValue]) => {
-          spread.props.set(propName, propValue)
-          boxByProp.set(propName, (boxByProp.get(propName) ?? []).concat(propValue))
-        })
-      }
-
-      const processBoxNode = (boxNode: BoxNode, isRootConditional?: boolean) => {
-        return match(boxNode)
-          .when(box.isConditional, (boxNode) => {
-            // when isRootConditional = true, i'm not sure if we even need to call processBoxNode on whenTrue & whenFalse
-            // we could maybe just push the boxNode as is in spread.conditionals ?
-            const whenTrue = processBoxNode(boxNode.whenTrue, isRootConditional)
-            const whenFalse = processBoxNode(boxNode.whenFalse, isRootConditional)
-
-            if (whenTrue && whenFalse) {
-              spread.conditionals.push(
-                box.conditional(whenTrue, whenFalse, boxNode.getNode(), boxNode.getStack(), boxNode.kind),
-              )
+          mapValue.forEach((propValue, propName) => {
+            if (isMap ? true : matchProp({ propName, propNode: node as any })) {
+              component.props.set(propName, propValue)
+              boxByProp.set(propName, (boxByProp.get(propName) ?? []).concat(propValue))
             }
           })
-          .when(Bool.or(box.isObject, box.isMap), (boxNode) => {
-            return processObjectLike(boxNode, isRootConditional)
-          })
-          .otherwise(noop)
+        }
+
+        const processBoxNode = (boxNode: BoxNode) => {
+          return (
+            match(boxNode)
+              // <ColorBox {...(someCondition && { color: "facebook.100" })} />
+              .when(box.isConditional, (boxNode) => {
+                component.conditionals.push(boxNode)
+              })
+              .when(Bool.or(box.isObject, box.isMap), (boxNode) => {
+                return processObjectLike(boxNode)
+              })
+              .otherwise(noop)
+          )
+        }
+
+        processBoxNode(spreadNode)
+
+        return
       }
 
-      processBoxNode(spreadNode, box.isConditional(spreadNode))
+      if (Node.isJsxAttribute(node)) {
+        // <ColorBox color="red.200" backgroundColor="blackAlpha.100" />
+        //           ^^^^^           ^^^^^^^^^^^^^^^
 
-      return
-    }
+        pipe(
+          componentNode,
+          Opt.fromNullable,
+          Opt.map((componentNode) => {
+            const componentName = getComponentName(componentNode)
+            const isFactory = componentName.includes('.')
+            return { componentNode, componentName, isFactory }
+          }),
+          Opt.filter(({ componentName, componentNode, isFactory }) =>
+            components.matchTag({ tagNode: componentNode, tagName: componentName, isFactory }),
+          ),
+          Opt.map(Obj.assign({ propName: node.getNameNode().getText() })),
+          Opt.filter(({ propName, componentNode, componentName }) =>
+            components.matchProp({ tagNode: componentNode, tagName: componentName, propName, propNode: node }),
+          ),
+          Opt.tap(({ propName, componentNode, componentName }) => {
+            pipe(
+              extractJsxAttribute(node, ctx),
+              Opt.fromNullable,
+              Opt.tap((maybeBox) => {
+                if (!byName.has(componentName)) {
+                  byName.set(componentName, { kind: 'component', nodesByProp: new Map(), queryList: [] })
+                }
 
-    if (components && Node.isJsxAttribute(node)) {
-      // <ColorBox color="red.200" backgroundColor="blackAlpha.100" />
-      //           ^^^^^           ^^^^^^^^^^^^^^^
+                if (!componentByNode.has(componentNode)) {
+                  componentByNode.set(componentNode, { name: componentName, props: new Map(), conditionals: [] })
+                }
+                const component = componentByNode.get(componentNode)!
+                const boxByProp = byName.get(componentName)!.nodesByProp
 
-      // TODO keep track of the JsxAttributes[element] index (including spread) to keep track of the order before & after the spread
-      // so we know which props can be overriden and avoid generating styles for those that were overriden
-
-      pipe(
-        node.getFirstAncestor(isJsxElement),
-        Opt.fromNullable,
-        Opt.map((componentNode) => {
-          const componentName = getComponentName(componentNode)
-          const isFactory = componentName.includes('.')
-          return { componentNode, componentName, isFactory }
-        }),
-        Opt.filter(({ componentName, componentNode, isFactory }) =>
-          components.matchTag({ tagNode: componentNode, tagName: componentName, isFactory }),
-        ),
-        Opt.map(Obj.assign({ propName: node.getNameNode().getText() })),
-        Opt.filter(({ propName, componentNode, componentName }) =>
-          components.matchProp({ tagNode: componentNode, tagName: componentName, propName, propNode: node }),
-        ),
-        Opt.tap(({ propName, componentNode, componentName }) => {
-          pipe(
-            extractJsxAttribute(node, ctx),
-            Opt.fromNullable,
-            Opt.tap((maybeBox) => {
-              if (!result.has(componentName)) {
-                result.set(componentName, { kind: 'component', nodesByProp: new Map(), queryList: [] })
-              }
-              const boxByProp = result.get(componentName)!.nodesByProp
-              boxByProp.set(propName, (boxByProp.get(propName) ?? []).concat(maybeBox))
-            }),
-            Opt.tap((maybeBox) => {
-              if (!componentMap.has(componentNode)) {
-                componentMap.set(componentNode, { name: componentName, props: new Map(), spreads: [] })
-              }
-              const parentRef = componentMap.get(componentNode)!
-              parentRef.props.set(propName, maybeBox)
-            }),
-          )
-        }),
-      )
+                component.props.set(propName, maybeBox)
+                boxByProp.set(propName, (boxByProp.get(propName) ?? []).concat(maybeBox))
+              }),
+            )
+          }),
+        )
+      }
     }
 
     if (functions && Node.isCallExpression(node)) {
@@ -205,11 +174,11 @@ export const extract = ({ ast, ...ctx }: ExtractOptions) => {
       const matchProp = ({ propName, propNode }: MatchFnPropArgs) =>
         functions.matchProp({ fnNode: node, fnName, propName, propNode })
 
-      if (!result.has(fnName)) {
-        result.set(fnName, { kind: 'function', nodesByProp: new Map(), queryList: [] })
+      if (!byName.has(fnName)) {
+        byName.set(fnName, { kind: 'function', nodesByProp: new Map(), queryList: [] })
       }
 
-      const fnResultMap = result.get(fnName)! as ExtractedFunctionResult
+      const fnResultMap = byName.get(fnName)! as ExtractedFunctionResult
       const boxByProp = fnResultMap.nodesByProp
 
       const boxNodeArray = extractCallExpressionArguments(node, ctx, matchProp, functions.matchArg)
@@ -219,23 +188,19 @@ export const extract = ({ ast, ...ctx }: ExtractOptions) => {
         Arr.map((boxNode) =>
           match(boxNode)
             .when(Bool.or(box.isObject, box.isMap), (boxNode) => {
-              const map = objectLikeToMap(boxNode, node)
+              const mapValue = objectLikeToMap(boxNode, node)
+              const isMap = box.isMap(boxNode)
 
-              const entries = mergeSpreads({
-                map,
+              mapValue.forEach((propValue, propName) => {
                 // if the boxNode is an object
                 // that means it was evaluated so we need to filter its props
                 // otherwise, it was already filtered in extractCallExpressionArguments
-                matchProp: box.isObject(boxNode) ? (matchProp as any) : undefined,
+                if (isMap ? true : matchProp({ propName, propNode: node as any })) {
+                  boxByProp.set(propName, (boxByProp.get(propName) ?? []).concat(propValue))
+                }
               })
 
-              const mapAfterSpread = new Map() as MapTypeValue
-              entries.forEach(([propName, propValue]) => {
-                mapAfterSpread.set(propName, propValue)
-                boxByProp.set(propName, (boxByProp.get(propName) ?? []).concat(propValue))
-              })
-
-              const boxMap = box.map(mapAfterSpread, node, boxNode.getStack())
+              const boxMap = box.map(mapValue, node, boxNode.getStack())
               if (box.isMap(boxNode) && boxNode.spreadConditions?.length) {
                 boxMap.spreadConditions = boxNode.spreadConditions
               }
@@ -251,66 +216,26 @@ export const extract = ({ ast, ...ctx }: ExtractOptions) => {
     }
   })
 
-  componentMap.forEach((parentRef, componentNode) => {
-    const componentName = parentRef.name
-    const queryList = (result.get(componentName)! as ExtractedComponentResult).queryList
+  // after traversing the whole tree
+  // since we targeted the component nodes (JsxAttribute/JsxSpreadAttribute) we didnt know when we were done with a component
+  // we can now reconstruct each component instance (a `query` is made of a component instance BoxNodeMap + its name)
+  componentByNode.forEach((parentRef, componentNode) => {
+    const component = componentByNode.get(componentNode)
+    if (!component) return
+
     const query = cast<ExtractedComponentInstance>({
       name: parentRef.name,
-      box: box.map(parentRef.props, componentNode, []),
+      box: box.map(component.props, componentNode, []),
     })
 
-    // TODO keep track of the JsxAttributes[element] index (including spread) to keep track of the order before & after the spread
-    // for now spread overrides everything (sorry)
-
-    // should have:
-    // <ColorBox color="red" padding="4" {...{ color: "blue.100" }} margin={2} /> => color: "blue.100"
-    // <ColorBox padding="4" {...{ color: "blue.100" }} color="red" margin={2} /> => color: "red"
-
-    // currently have:
-    // <ColorBox color="red" padding="4" {...{ color: "blue.100" }} margin={2} /> => color: "blue.100"
-    // <ColorBox padding="4" {...{ color: "blue.100" }} color="red" margin={2} /> => color: "blue.100"
-    const spread = spreadByComponent.get(componentNode)
-    if (spread) {
-      spread.props.forEach((propNode, propName) => {
-        query.box.value.set(propName, propNode)
-      })
-
-      if (spread.conditionals?.length) {
-        if (query.box.spreadConditions) {
-          query.box.spreadConditions = query.box.spreadConditions.concat(spread.conditionals)
-        } else {
-          query.box.spreadConditions = spread.conditionals
-        }
-      }
+    if (component.conditionals?.length) {
+      query.box.spreadConditions = component.conditionals
     }
 
+    const componentName = parentRef.name
+    const queryList = (byName.get(componentName)! as ExtractedComponentResult).queryList
     queryList.push(query)
   })
 
-  return result
-}
-
-/**
- * reverse prop entries so that the last one wins
- * @example <Box sx={{ ...{ color: "red" }, color: "blue" }} />
- * // color: "blue" wins / color: "red" is ignored
- */
-function mergeSpreads({ map, matchProp }: { map: MapTypeValue; matchProp?: (prop: MatchFnPropArgs) => boolean }) {
-  if (map.size <= 1) {
-    return Array.from(map.entries())
-  }
-
-  const foundPropList = new Set<string>()
-
-  const merged = Array.from(map.entries())
-    .reverse()
-    .filter(([propName, boxed]) => {
-      if (matchProp && !matchProp?.({ propName, propNode: boxed.getNode() as any })) return false
-      if (foundPropList.has(propName)) return false
-      foundPropList.add(propName)
-      return true
-    })
-
-  // reverse again to keep the original order
-  return merged.reverse()
+  return byName
 }
