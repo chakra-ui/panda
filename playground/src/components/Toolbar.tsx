022e2c02de29024e7d4f1c71979fb61126fd02cd
Segun Adebayo <joseshegs@gmail.com>
2023-04-15 09:48:18 +0100

    fix: playground styles

--- playground/src/components/Toolbar.tsx
+++ playground/src/components/Toolbar.tsx
@@ -5,7 +5,7 @@ import { Logo } from './Logo'
 export const Toolbar = (props: PropsWithChildren) => (
   <HStack px="6" minH="16" borderBottomWidth="1px" gap="6">
     <Logo />
-    <HStack flex="1" justifyContent="space-between">
+    <HStack flex="1" justify="space-between">
       {props.children}
     </HStack>
   </HStack>
