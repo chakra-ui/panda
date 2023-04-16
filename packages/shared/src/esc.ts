f9e5983cac58a8f453cfe57edb18466ed2cc8b51
Segun Adebayo <joseshegs@gmail.com>
2023-04-16 20:16:46 +0100

    perf: optimize esc regex

--- packages/shared/src/esc.ts
+++ packages/shared/src/esc.ts
@@ -1,4 +1,5 @@
+const escapeRegExp = /(^[^_a-zA-Z\u00a0-\uffff]|[^-_a-zA-Z0-9\u00a0-\uffff])/g
 export function esc(str: string) {
   //@ts-ignore
-  return ''.replace.call(str, /(^[^_a-zA-Z\u00a0-\uffff]|[^-_a-zA-Z0-9\u00a0-\uffff])/g, '\\$1')
+  return ''.replace.call(str, escapeRegExp, '\\$1')
 }
