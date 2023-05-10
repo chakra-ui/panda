93c33f9ecc0fce62822ae6df3f3769be2f3190e9
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-05-11 01:43:04 +0200

    chore: rm old fixtures files

--- packages/fixture/src/text-styles.ts
+++
@@ -1,6 +0,0 @@
-import type { TextStyles } from '@pandacss/types'
-
-export const textStyles: TextStyles = {
-  xs: { value: { fontSize: '0.75rem', lineHeight: '1rem' } },
-  md: { value: { fontSize: '0.875rem', lineHeight: '1.25rem' } },
-}
