4febaa7246831deca17e2ded1b047689ec5eea0e
Ivica Batinic <ivica.batinic@infinum.hr>
2023-05-20 17:35:11 +0200

    Init postcss and cli sandboxes

--- sandbox/next-js/panda.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@pandacss/dev'
-
-export default defineConfig({
-  presets: ['@pandacss/dev/presets'],
-  outdir: 'styled-system',
-  include: ['pages/**/*.jsx'],
-  jsxFramework: 'react',
-})
