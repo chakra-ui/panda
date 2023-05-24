dd2210c747021a56abae9aba0c8df8f0a9913c1b
Ivica Batinic <ivica.batinic@infinum.hr>
2023-05-24 10:59:43 +0200

    Finish steps styles

--- website/src/nextra-components/steps.tsx
+++ website/src/nextra-components/steps.tsx
@@ -1,46 +1,6 @@
 import type { ComponentProps, ReactElement } from 'react'
-import { cva, cx } from '../../styled-system/css'
+import { css, cx } from '../../styled-system/css'
 
-const steps = cva({
-  base: {
-    ml: 4,
-    mb: 12,
-    borderLeft: '1px solid token(colors.gray.200)',
-    pl: 6,
-    counterReset: 'step',
-    h3: {
-      // @ts-ignore
-      counterIncrement: 'step',
-      _before: {
-        h: '33px',
-        w: '33px',
-        borderWidth: '4px',
-        borderColor: 'white',
-        backgroundColor: 'yellow.300',
-        position: 'absolute',
-        textAlign: 'center',
-        textIndent: '-1px',
-        color: 'black',
-        content: 'counter(step)',
-        borderRadius: 'full',
-        mt: '3px',
-        ml: '-41px',
-        fontSize: '1rem',
-        fontWeight: '400'
-      }
-    },
-    _dark: {
-      borderColor: 'neutral.800',
-      h3: {
-        _before: {
-          // @ts-ignore
-          borderColor: 'rgba(17,17,17,1)',
-          backgroundColor: 'rgba(38,38,38,1)'
-        }
-      }
-    }
-  }
-});
 
 export function Steps({
   children,
@@ -51,7 +11,42 @@ export function Steps({
     <div
       className={cx(
         'nextra-steps',
-        steps({}),
+        css({
+          ml: 4,
+          mb: 12,
+          borderLeft: '1px solid token(colors.gray.200)',
+          pl: 6,
+          counterReset: 'step',
+          '& > h3': {
+            counterIncrement: 'step',
+            _before: {
+              h: '33px',
+              w: '33px',
+              borderWidth: '4px',
+              borderColor: 'white',
+              backgroundColor: 'yellow.300',
+              position: 'absolute',
+              textAlign: 'center',
+              textIndent: '-1px',
+              color: 'black',
+              content: 'counter(step)',
+              borderRadius: 'full',
+              mt: '3px',
+              ml: '-41px',
+              fontSize: 'md',
+              fontWeight: 'bold',
+              lineHeight: 'relaxed',
+            }
+          },
+          _dark: {
+            borderColor: 'neutral.800',
+            '& > h3': {
+              _before: {
+                borderColor: 'rgba(17,17,17,1)',
+              }
+            }
+          }
+        }),
         className
       )}
       {...props}
