120cd2dfcbb58abd2f1b444bc675d25b128dfe7c
Ivica Batinic <ivica.batinic@infinum.hr>
2023-05-25 17:32:10 +0200

    Remove nextjs cli sandbox

--- sandbox/next-js-cli/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { css } from '../../styled-system/css';
-
-export default function Home() {
-  return (
-    <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
-  )
-}
