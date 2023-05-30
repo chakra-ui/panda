9c2c7ea1d4935d30e014ca807a4f9cb1665b1e41
Christian Schröter <dev@grizzly.codes>
2023-05-30 09:28:40 +0200

    feat(playground): allow users to share their playground session

--- playground/prisma/migrations/20230204163131_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Session" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "code" TEXT NOT NULL,
-    "config" TEXT NOT NULL,
-    "view" TEXT NOT NULL DEFAULT 'code',
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
