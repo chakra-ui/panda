c9e7a25e8661782a8fb52c61f8fca4c373f9a57c
Segun Adebayo <joseshegs@gmail.com>
2023-06-09 16:42:33 +0100

    docs: refactor getting started

---
+++ website/src/icons/framework.solid.tsx
@@ -0,0 +1,84 @@
+export const SolidjsLogo = () => (
+  <svg
+    width="40"
+    height="40"
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <linearGradient
+      id="solidjs_svg__a"
+      gradientUnits="userSpaceOnUse"
+      x1="9.301"
+      y1="5.578"
+      x2="33.301"
+      y2="17.241"
+    >
+      <stop offset="0.1" stopColor="#76B3E1"></stop>
+      <stop offset="0.3" stopColor="#DCF2FD"></stop>
+      <stop offset="1" stopColor="#76B3E1"></stop>
+    </linearGradient>
+    <linearGradient
+      id="solidjs_svg__b"
+      gradientUnits="userSpaceOnUse"
+      x1="22.468"
+      y1="11.284"
+      x2="18.265"
+      y2="25.279"
+    >
+      <stop offset="0" stopColor="#76B3E1"></stop>
+      <stop offset="0.5" stopColor="#4377BB"></stop>
+      <stop offset="1" stopColor="#1F3B77"></stop>
+    </linearGradient>
+    <linearGradient
+      id="solidjs_svg__c"
+      gradientUnits="userSpaceOnUse"
+      x1="7.547"
+      y1="17.376"
+      x2="31.817"
+      y2="33.877"
+    >
+      <stop offset="0" stopColor="#315AA9"></stop>
+      <stop offset="0.5" stopColor="#518AC8"></stop>
+      <stop offset="1" stopColor="#315AA9"></stop>
+    </linearGradient>
+    <linearGradient
+      id="solidjs_svg__d"
+      gradientUnits="userSpaceOnUse"
+      x1="18.496"
+      y1="19.361"
+      x2="8.704"
+      y2="55.275"
+    >
+      <stop offset="0" stopColor="#4377BB"></stop>
+      <stop offset="0.5" stopColor="#1A336B"></stop>
+      <stop offset="1" stopColor="#1A336B"></stop>
+    </linearGradient>
+    <path
+      fill="#76B3E1"
+      d="M35.422 11.747s-10.217-7.518-18.12-5.783l-.58.193c-1.156.385-2.12.963-2.698 1.735l-.385.578-2.892 5.012 5.012.964c2.12 1.35 4.82 1.928 7.325 1.35l8.868 1.734 3.47-5.783z"
+    ></path>
+    <path
+      fill="url(#solidjs_svg__a)"
+      opacity="0.3"
+      d="M35.422 11.747s-10.217-7.518-18.12-5.783l-.58.193c-1.156.385-2.12.963-2.698 1.735l-.385.578-2.892 5.012 5.012.964c2.12 1.35 4.82 1.928 7.325 1.35l8.868 1.734 3.47-5.783z"
+    ></path>
+    <path
+      fill="#518AC8"
+      d="M14.024 11.747l-.771.193c-3.277.964-4.241 4.048-2.506 6.747 1.928 2.506 5.976 3.855 9.253 2.891l11.952-4.048s-10.217-7.518-17.928-5.783z"
+    ></path>
+    <path
+      fill="url(#solidjs_svg__b)"
+      opacity="0.3"
+      d="M14.024 11.747l-.771.193c-3.277.964-4.241 4.048-2.506 6.747 1.928 2.506 5.976 3.855 9.253 2.891l11.952-4.048s-10.217-7.518-17.928-5.783z"
+    ></path>
+    <path
+      fill="url(#solidjs_svg__c)"
+      d="M29.831 20.422a8.675 8.675 0 00-9.253-2.892L8.626 21.385l-3.855 6.747 21.59 3.663 3.856-6.94c.77-1.349.578-2.891-.386-4.433z"
+    ></path>
+    <path
+      fill="url(#solidjs_svg__d)"
+      d="M25.976 27.169a8.675 8.675 0 00-9.253-2.892L4.77 28.132s10.217 7.711 18.12 5.784l.579-.193c3.277-.964 4.434-4.048 2.506-6.554z"
+    ></path>
+  </svg>
+)
---
+++ website/src/icons/framework.vite.tsx
@@ -0,0 +1,43 @@
+export const ViteLogo = () => (
+  <svg
+    width="40"
+    height="40"
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M31.881 11.61L20.886 31.563a.593.593 0 01-1.04.004L8.633 11.611c-.251-.446.125-.987.624-.897l11.007 1.997a.59.59 0 00.212 0l10.777-1.994c.497-.092.875.445.628.893z"
+      fill="url(#vite_svg__paint0_linear_41_6732)"
+    />
+    <path
+      d="M25.506 8.096l-8.137 1.618a.302.302 0 00-.241.28l-.5 8.578a.3.3 0 00.365.314l2.265-.531c.212-.05.404.14.36.356l-.673 3.345a.3.3 0 00.38.35l1.399-.43a.3.3 0 01.38.35l-1.07 5.255c-.067.328.364.507.543.226l.12-.189 6.63-13.428c.111-.225-.08-.481-.323-.433l-2.332.456a.301.301 0 01-.344-.381l1.522-5.355a.301.301 0 00-.345-.381z"
+      fill="url(#vite_svg__paint1_linear_41_6732)"
+    />
+    <defs>
+      <linearGradient
+        id="vite_svg__paint0_linear_41_6732"
+        x1="8.359"
+        y1="10.001"
+        x2="22.306"
+        y2="28.665"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#41D1FF"></stop>
+        <stop offset="1" stopColor="#BD34FE"></stop>
+      </linearGradient>
+      <linearGradient
+        id="vite_svg__paint1_linear_41_6732"
+        x1="19.631"
+        y1="8.535"
+        x2="22.178"
+        y2="25.757"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#FFEA83"></stop>
+        <stop offset="0.083" stopColor="#FFDD35"></stop>
+        <stop offset="1" stopColor="#FFA800"></stop>
+      </linearGradient>
+    </defs>
+  </svg>
+)
