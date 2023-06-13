dc1c4e661932a837b688a14ed9ce3ea32a014d55
Segun Adebayo <joseshegs@gmail.com>
2023-06-13 16:04:08 +0100

    feat: support granular control of hash and prefix

--- packages/generator/src/engines/base.ts
+++ packages/generator/src/engines/base.ts
@@ -1,10 +1,18 @@
-import { Conditions, Recipes, Stylesheet, Utility, assignCompositions, type StylesheetOptions } from '@pandacss/core'
+import {
+  Conditions,
+  Recipes,
+  Stylesheet,
+  Utility,
+  assignCompositions,
+  type StylesheetContext,
+  type StylesheetOptions,
+} from '@pandacss/core'
 import { isCssProperty } from '@pandacss/is-valid-prop'
 import { logger } from '@pandacss/logger'
 import { compact, mapObject, memo } from '@pandacss/shared'
 import { TokenDictionary } from '@pandacss/token-dictionary'
 import type { LoadConfigResult } from '@pandacss/types'
-import { Obj, pipe, tap } from 'lil-fp'
+import { Obj, isBool, isStr, pipe, tap } from 'lil-fp'
 import postcss from 'postcss'
 
 const helpers = { map: mapObject }
@@ -15,17 +23,39 @@ export const getBaseEngine = (conf: LoadConfigResult) =>
 
     Obj.bind('configDependencies', ({ dependencies }) => dependencies ?? []),
 
-    Obj.bind('tokens', ({ config: { theme = {}, prefix, hash } }) => {
+    Obj.assign(({ config: { hash, prefix } }) => ({
+      hash: {
+        tokens: isBool(hash) ? hash : hash?.cssVar,
+        className: isBool(hash) ? hash : hash?.className,
+      },
+      prefix: {
+        tokens: isStr(prefix) ? prefix : prefix?.cssVar,
+        className: isStr(prefix) ? prefix : prefix?.className,
+      },
+    })),
+
+    Obj.bind('tokens', ({ hash, prefix, config: { theme = {} } }) => {
       const { breakpoints, tokens, semanticTokens } = theme
-      return new TokenDictionary({ breakpoints, tokens, semanticTokens, prefix, hash })
+      return new TokenDictionary({
+        breakpoints,
+        tokens,
+        semanticTokens,
+        prefix: prefix.tokens,
+        hash: hash.tokens,
+      })
     }),
 
     tap(({ tokens }) => {
       logger.debug('generator:tokens', tokens.allNames)
     }),
 
-    Obj.bind('utility', ({ config: { prefix, utilities, separator }, tokens }) => {
-      return new Utility({ prefix, tokens, config: utilities, separator })
+    Obj.bind('utility', ({ config: { utilities, separator }, tokens, prefix }) => {
+      return new Utility({
+        prefix: prefix.className,
+        tokens,
+        config: utilities,
+        separator,
+      })
     }),
 
     Obj.bind('conditions', ({ config: { conditions, theme } }) => {
@@ -34,15 +64,23 @@ export const getBaseEngine = (conf: LoadConfigResult) =>
     }),
 
     tap(({ conditions, utility, config: { theme } }) => {
-      logger.debug('generator:conditions', conditions)
-
       const { textStyles, layerStyles } = theme ?? {}
       const compositions = compact({ textStyle: textStyles, layerStyle: layerStyles })
       assignCompositions({ conditions, utility }, compositions)
     }),
 
-    Obj.bind('recipes', ({ conditions, utility, config: { hash, theme } }) => {
-      const context = { root: postcss.root(), conditions, utility, hash, helpers }
+    Obj.bind('createSheetContext', ({ conditions, utility, hash }) => {
+      return (): StylesheetContext => ({
+        root: postcss.root(),
+        conditions,
+        utility,
+        hash: hash.className,
+        helpers,
+      })
+    }),
+
+    Obj.bind('recipes', ({ config: { theme }, createSheetContext }) => {
+      const context = createSheetContext()
       const recipes = new Recipes(theme?.recipes ?? {}, context)
       recipes.save() // cache recipes on first run
       return recipes
@@ -50,9 +88,9 @@ export const getBaseEngine = (conf: LoadConfigResult) =>
 
     Obj.bind(
       'createSheet',
-      ({ conditions, utility, config: { hash, theme } }) =>
+      ({ config: { theme }, createSheetContext }) =>
         (options?: Pick<StylesheetOptions, 'content'>) => {
-          const context = { root: postcss.root(), conditions, utility, hash, helpers }
+          const context = createSheetContext()
           return new Stylesheet(context, {
             content: options?.content,
             recipes: theme?.recipes,
