06ee5243ec8e6b2c2f16410b65999a0de00838e9
Segun Adebayo <joseshegs@gmail.com>
2023-06-21 15:02:16 +0100

    docs: refactor styles

--- website/src/nextra/treeview/treeview-list.tsx
+++ website/src/nextra/treeview/treeview-list.tsx
@@ -6,33 +6,22 @@ const threeViewListStyle = cva({
     position: 'relative',
     display: 'flex',
     flexDirection: 'column',
-    gap: 1,
+    gap: '1'
   },
   variants: {
     root: {
       false: {
+        paddingStart: '3',
+        marginStart: '3',
         _before: {
           position: 'absolute',
-          insetY: 1,
+          insetY: '1',
           width: '1px',
           bg: 'gray.200',
           content: "''",
+          insetStart: '0',
           _dark: {
             bg: 'whiteAlpha.200'
-          }
-        },
-        _ltr: {
-          pl: 3,
-          ms: 3,
-          _before: {
-            left: 0
-          }
-        },
-        _rtl: {
-          pr: 3,
-          me: 3,
-          _before: {
-            right: 0
           }
         }
       }
@@ -43,8 +32,6 @@ const threeViewListStyle = cva({
   }
 })
 
-export type TreeViewListVariants = RecipeVariantProps<
-  typeof threeViewListStyle
->
+export type TreeViewListVariants = RecipeVariantProps<typeof threeViewListStyle>
 
 export const TreeViewList = panda('ul', threeViewListStyle)
