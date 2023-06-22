6b9489b92d8957de77e5d328236cea99a228b77e
Segun Adebayo <joseshegs@gmail.com>
2023-06-22 09:24:44 +0100

    feat: add astish fn

---
+++ packages/shared/__tests__/astish.test.ts
@@ -0,0 +1,34 @@
+import { describe, expect, test } from 'vitest'
+import { astish } from '../src/astish'
+
+describe('astish', () => {
+  test('should work', () => {
+    const result = astish(`
+        display: flex;
+        align-items: center;
+        -webkit-align-items: center;
+        @media (min-width: 400){
+        color: red;
+        justify-content: center;
+        }
+        @container (min-inline-width: 600px){
+        background: pink;
+        }
+`)
+
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "@container (min-inline-width: 600px)": {
+          "background": "pink",
+        },
+        "@media (min-width: 400)": {
+          "color": "red",
+          "justifyContent": "center",
+        },
+        "WebkitAlignItems": "center",
+        "alignItems": "center",
+        "display": "flex",
+      }
+    `)
+  })
+})
