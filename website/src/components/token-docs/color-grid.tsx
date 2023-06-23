beda57df909a732985137f91722cf676ec5f3109
anubra266 <anubra266@gmail.com>
2023-06-23 13:37:54 +0100

    chore: allow copying colors

--- website/src/components/token-docs/color-grid.tsx
+++ website/src/components/token-docs/color-grid.tsx
@@ -1,22 +1,47 @@
-import { css } from '@/styled-system/css'
+import { useClipboard } from '@/mdx/use-clipboard'
+import { css, cva } from '@/styled-system/css'
 import { grid, hstack, square } from '@/styled-system/patterns'
 import { Token } from '@pandacss/token-dictionary'
 
 export const ColorGrid = ({ tokens }: { tokens: Token[] }) => (
-  <div className={grid({ columns: 3, gap: '2', fontSize: 'sm' })}>
+  <div className={grid({ minChildWidth: '11rem', gap: '2', fontSize: 'sm' })}>
     {tokens.map((token, index) => (
-      <div key={index} className={hstack()}>
-        <div
-          className={square({ size: '8', rounded: 'sm' })}
-          style={{ background: token.extensions.varRef }}
-        />
-        <div>
-          <div className={css({ fontWeight: 'medium' })}>
-            {token.extensions.prop}
-          </div>
-          <div className={css({ opacity: '0.6' })}>{token.value}</div>
-        </div>
-      </div>
+      <ColorGridToken token={token} key={index} />
     ))}
   </div>
 )
+
+export const ColorGridToken = ({ token }: { token: Token }) => {
+  const { isCopied, copy } = useClipboard({ getValue: () => token.value })
+
+  return (
+    <div className={hstack({ cursor: 'pointer' })} onClick={copy}>
+      <div
+        className={square({ size: '8', rounded: 'sm' })}
+        style={{ background: token.extensions.varRef }}
+      />
+      <div>
+        <div className={css({ fontWeight: 'medium' })}>
+          {token.extensions.prop}
+        </div>
+        <div className={colorTokenValue({ copied: isCopied })}>
+          {token.value}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const colorTokenValue = cva({
+  base: {
+    opacity: '0.6'
+  },
+  variants: {
+    copied: {
+      true: {
+        opacity: '1',
+        color: 'green.500'
+      }
+    }
+  }
+})
