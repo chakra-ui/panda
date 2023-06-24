f155160cd06cdd005609c590cf7c8da8f58c3473
Ivica Batinic <ivica.batinic@infinum.hr>
2023-06-24 17:02:31 +0200

    refactor: move nextra tabs to cva and change the styles

--- website/src/nextra/tabs/tabs.tsx
+++ website/src/nextra/tabs/tabs.tsx
@@ -1,5 +1,5 @@
-import { cx } from '@/styled-system/css'
-import { TabsVariantProps, tabs as tabsRecipe } from '@/styled-system/recipes'
+import { cva, cx } from '@/styled-system/css'
+import { TabsVariantProps } from '@/styled-system/recipes'
 import {
   Tabs as ArkTabs,
   TabContent,
@@ -9,6 +9,72 @@ import {
 } from '@ark-ui/react'
 import { Children, cloneElement } from 'react'
 import { IUseTabsProps, useTabs } from './use-tabs'
+
+const tabsStyles = cva({
+  base: {
+    '&[data-scope="tabs"][data-part="root"]': {
+      overflowX: 'auto',
+      overflowY: 'hidden',
+      overscrollBehaviorX: 'contain'
+    },
+    '& [data-scope="tabs"][data-part="tablist"]': {
+      position: 'relative',
+      mt: '4',
+      display: 'flex',
+      w: 'max',
+      minW: 'full',
+      p: 1,
+      bg: 'gray.200',
+      borderTopLeftRadius: 'xl',
+      borderTopRightRadius: 'xl',
+      _dark: {
+        bg: 'gray.700'
+      }
+    },
+    '& [data-scope="tabs"][data-part="trigger"]': {
+      py: '1px',
+      px: 2,
+      lineHeight: '1.5rem',
+      transitionProperty: 'colors',
+      whiteSpace: 'nowrap',
+      userSelect: 'none',
+      color: 'gray.600',
+      zIndex: 1,
+      fontSize: 'sm',
+      fontWeight: 'medium',
+      _hover: {
+        color: 'gray.900'
+      },
+      _dark: {
+        color: 'gray.300',
+        _hover: {
+          color: 'white',
+        },
+        _selected: {
+          color: 'black !important',
+        }
+      },
+      _selected: {
+        color: 'black',
+      }
+    },
+    '& [data-scope="tabs"][data-part="indicator"]': {
+      position: 'absolute',
+      inset: 1,
+      bg: 'yellow.300',
+      borderRadius: 'md',
+      zIndex: 0,
+    },
+    '& [data-scope="tabs"][data-part="content"]': {
+      bg: 'gray.100',
+      borderBottomLeftRadius: 'xl',
+      borderBottomRightRadius:'xl',
+      _dark: {
+        bg: 'gray.800'
+      }
+    }
+  }
+})
 
 export interface ITabProps extends TabsVariantProps, IUseTabsProps {
   children: React.ReactElement[]
@@ -22,7 +88,7 @@ export function Tabs(props: ITabProps) {
     <ArkTabs
       value={value}
       onChange={onChange}
-      className={cx('nextra-scrollbar', tabsRecipe({ variant }))}
+      className={cx('nextra-scrollbar', tabsStyles({ variant }))}
     >
       <TabList>
         {items.map((item, index) => {
@@ -50,7 +116,5 @@ export interface ITabProps extends React.ComponentProps<'div'> {
 }
 
 export function Tab({ children, ...props }: ITabProps) {
-  return (
-    <TabContent {...props}>{children}</TabContent>
-  )
+  return <TabContent {...props}>{children}</TabContent>
 }
