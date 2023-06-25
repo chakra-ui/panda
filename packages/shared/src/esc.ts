b94ae14d4835ad7a02dc216855ec24c653be7e52
JB Hutch <jbhutch01@gmail.com>
2023-06-25 18:45:01 -0400

    Update esc to properly escape classnames starting with number

--- packages/shared/src/esc.ts
+++ packages/shared/src/esc.ts
@@ -1,5 +1,25 @@
+import cssesc from 'cssesc'
+
 const escapeRegExp = /(^[^_a-zA-Z\u00a0-\uffff]|[^-_a-zA-Z0-9\u00a0-\uffff])/g
 export function esc(str: string) {
   //@ts-ignore
-  return ''.replace.call(str, escapeRegExp, '\\$1')
+
+  // Option A
+  const classNameA = cssesc(str, { isIdentifier: true })
+
+  // Option C
+  let classNameB = str
+  if (/^\d/.test(str)) {
+    const leadingDigit = str.charAt(0)
+    const leadingDigitCode = leadingDigit.charCodeAt(0)
+
+    // Escape the leading digit using its Unicode equivalent
+    classNameB = classNameB.replace(/^\d/, leadingDigitCode.toString(16).toUpperCase())
+  }
+
+  classNameB = classNameB.replace(escapeRegExp, '\\$1')
+
+  console.log({ classNameA, classNameB })
+
+  return classNameB
 }
