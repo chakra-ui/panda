d33bf7029205691ddd13da946fe6dfe7f7110677
freddie <rincofrederico@gmail.com>
2023-07-05 15:39:44 +0100

    feat(types): make RecipeVariantProps only optional for variants missing defaults

--- packages/types/src/recipe.ts
+++ packages/types/src/recipe.ts
@@ -6,29 +6,37 @@ type StringToBoolean<T> = T extends 'true' | 'false' ? boolean : T
 
 export type RecipeVariantRecord = Record<any, Record<any, SystemStyleObject>>
 
-export type RecipeSelection<T extends RecipeVariantRecord> = keyof any extends keyof T
-  ? {}
-  : {
-      [K in keyof T]?: StringToBoolean<keyof T[K]>
+export type RecipeSelection<T extends RecipeVariantRecord, D extends RecipeSelection<T, any> = {}> = D extends Record<
+  string,
+  unknown
+>
+  ? { [K in keyof D]?: StringToBoolean<keyof T[K]> } & {
+      [K in Exclude<keyof T, keyof D>]-?: StringToBoolean<keyof T[K]>
     }
+  : any
 
-export type RecipeVariantFn<T extends RecipeVariantRecord> = (props?: RecipeSelection<T>) => string
+export type RecipeVariantFn<T extends RecipeVariantRecord, D extends RecipeSelection<T, any>> = (
+  props?: RecipeSelection<T, D>,
+) => string
 
-export type RecipeVariantProps<T extends RecipeVariantFn<RecipeVariantRecord>> = Pretty<Parameters<T>[0]>
+export type RecipeVariantProps<T extends RecipeVariantFn<any, any>> = Pretty<Parameters<T>[0]>
 
 type RecipeVariantMap<T extends RecipeVariantRecord> = {
   [K in keyof T]: Array<keyof T[K]>
 }
 
-export type RecipeRuntimeFn<T extends RecipeVariantRecord> = RecipeVariantFn<T> & {
-  __type: RecipeSelection<T>
+export type RecipeRuntimeFn<T extends RecipeVariantRecord, D extends RecipeSelection<T, any>> = RecipeVariantFn<
+  T,
+  D
+> & {
+  __type: RecipeSelection<T, D>
   variantKeys: (keyof T)[]
   variantMap: RecipeVariantMap<T>
-  resolve: (props: RecipeSelection<T>) => SystemStyleObject
+  resolve: (props: RecipeSelection<T, D>) => SystemStyleObject
   config: RecipeConfig<T>
-  splitVariantProps<Props extends RecipeSelection<T>>(
+  splitVariantProps<Props extends RecipeSelection<T, D>>(
     props: Props,
-  ): [RecipeSelection<T>, Pretty<Omit<Props, keyof RecipeVariantRecord>>]
+  ): [RecipeSelection<T, D>, Pretty<Omit<Props, keyof RecipeVariantRecord>>]
 }
 
 export type RecipeCompoundSelection<T extends RecipeVariantRecord> = {
@@ -39,7 +47,7 @@ export type RecipeCompoundVariant<T extends RecipeVariantRecord> = RecipeCompoun
   css: SystemStyleObject
 }
 
-export type RecipeDefinition<T extends RecipeVariantRecord> = {
+export type RecipeDefinition<T extends RecipeVariantRecord, D extends RecipeSelection<T, any>> = {
   /**
    * The base styles of the recipe.
    */
@@ -51,16 +59,21 @@ export type RecipeDefinition<T extends RecipeVariantRecord> = {
   /**
    * The default variants of the recipe.
    */
-  defaultVariants?: RecipeSelection<T>
+  defaultVariants?: D
   /**
    * The styles to apply when a combination of variants is selected.
    */
   compoundVariants?: Array<RecipeCompoundVariant<T>>
 }
 
-export type RecipeCreatorFn = <T extends RecipeVariantRecord>(config: RecipeDefinition<T>) => RecipeRuntimeFn<T>
+export type RecipeCreatorFn = <T extends RecipeVariantRecord, D extends RecipeSelection<T, any>>(
+  config: RecipeDefinition<T, D>,
+) => RecipeRuntimeFn<T, D>
 
-export type RecipeConfig<T extends RecipeVariantRecord = RecipeVariantRecord> = RecipeDefinition<T> & {
+export type RecipeConfig<
+  T extends RecipeVariantRecord = RecipeVariantRecord,
+  D extends RecipeSelection<T, any> = RecipeSelection<T, any>,
+> = RecipeDefinition<T, D> & {
   /**
    * The name of the recipe.
    */
