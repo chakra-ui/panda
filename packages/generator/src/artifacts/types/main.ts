ebe3374fa8ebe797b07eaaf5cb1ace419497d2a4
Segun Adebayo <joseshegs@gmail.com>
2023-07-30 22:50:29 +0100

    fix: ambient types

--- packages/generator/src/artifacts/types/main.ts
+++ packages/generator/src/artifacts/types/main.ts
@@ -2,7 +2,7 @@ import { outdent } from 'outdent'
 
 export const generateTypesEntry = () => ({
   global: outdent`
-    import type { RecipeVariantRecord, RecipeConfig } from './recipe'
+    import type { RecipeVariantRecord, RecipeConfig, SlotRecipeVariantRecord, SlotRecipeConfig } from './recipe'
     import type { Parts } from './parts'
     import type { PatternConfig, PatternProperties } from './pattern'
     import type { GlobalStyleObject, SystemStyleObject } from './system-types'
@@ -10,12 +10,12 @@ export const generateTypesEntry = () => ({
 
     declare module '@pandacss/dev' {
       export function defineRecipe<V extends RecipeVariantRecord>(config: RecipeConfig<V>): RecipeConfig
+      export function defineSlotRecipe<S extends string, V extends SlotRecipeVariantRecord<S>>(config: SlotRecipeConfig<S, V>): SlotRecipeConfig
       export function defineStyles(definition: SystemStyleObject): SystemStyleObject
       export function defineGlobalStyles(definition: GlobalStyleObject): GlobalStyleObject
       export function defineTextStyles(definition: CompositionStyles['textStyles']): CompositionStyles['textStyles']
       export function defineLayerStyles(definition: CompositionStyles['layerStyles']): CompositionStyles['layerStyles']
       export function definePattern<T extends PatternProperties>(config: PatternConfig<T>): PatternConfig
-      export function defineParts<T extends Parts>(parts: T): (config: Partial<Record<keyof T, SystemStyleObject>>) => Partial<Record<keyof T, SystemStyleObject>>;
     }
     `,
   index: outdent`
