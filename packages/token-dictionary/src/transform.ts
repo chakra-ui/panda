0e7b52424389716cd72f8c3c9750c74037c219b8
Segun Adebayo <joseshegs@gmail.com>
2023-08-07 18:47:46 +0100

    fix: apply quote treatment to asset tokens

--- packages/token-dictionary/src/transform.ts
+++ packages/token-dictionary/src/transform.ts
@@ -149,7 +149,7 @@ export const transformAssets: TokenTransformer = {
     const raw = token.value as TokenDataTypes['assets']
     return match(raw)
       .with(P.string, (value) => value)
-      .with({ type: 'url' }, ({ value }) => `url(${value})`)
+      .with({ type: 'url' }, ({ value }) => `url('${value}')`)
       .with({ type: 'svg' }, ({ value }) => `url('${svgToDataUri(value)})'`)
       .exhaustive()
   },
