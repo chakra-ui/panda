b86776cb8f679a454e30dd1e623077425ebfd9d4
anubra266 <anubra266@gmail.com>
2023-08-16 22:56:35 +0100

    chore: fix remix sandbox

--- sandbox/remix/app/root.tsx
+++ sandbox/remix/app/root.tsx
@@ -1,20 +1,19 @@
-import type { LinksFunction, MetaFunction } from '@remix-run/node'
+import { cssBundleHref } from '@remix-run/css-bundle'
+import type { LinksFunction } from '@remix-run/node'
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
-
 import styles from './index.css'
 
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
-
-export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'New Remix App',
-  viewport: 'width=device-width,initial-scale=1',
-})
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+  { rel: 'stylesheet', href: styles },
+]
 
 export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
