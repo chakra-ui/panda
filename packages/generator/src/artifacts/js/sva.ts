a84b4490cb52d152b1465207ff1ce6cbcd54c54a
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-08-30 12:42:07 +0200

    feat(sva): add .raw fn

--- packages/generator/src/artifacts/js/sva.ts
+++ packages/generator/src/artifacts/js/sva.ts
@@ -9,9 +9,14 @@ export function generateSvaFn(ctx: Context) {
 
     export function sva(config) {
       const slots = Object.entries(getSlotRecipes(config)).map(([slot, slotCva]) => [slot, cva(slotCva)])
-      
+
       function svaFn(props) {
         const result = slots.map(([slot, cvaFn]) => [slot, cvaFn(props)])
+        return Object.fromEntries(result)
+      }
+
+      function raw(props) {
+        const result = slots.map(([slot, cvaFn]) => [slot, cvaFn.raw(props)])
         return Object.fromEntries(result)
       }
 
@@ -28,6 +33,7 @@ export function generateSvaFn(ctx: Context) {
 
       return Object.assign(svaFn, {
         __cva__: false,
+        raw,
         variantMap,
         variantKeys,
         splitVariantProps,
--- packages/studio/styled-system/css/sva.mjs
+++ packages/studio/styled-system/css/sva.mjs
@@ -3,9 +3,14 @@ import { cva } from './cva.mjs';
 
 export function sva(config) {
   const slots = Object.entries(getSlotRecipes(config)).map(([slot, slotCva]) => [slot, cva(slotCva)])
-  
+
   function svaFn(props) {
     const result = slots.map(([slot, cvaFn]) => [slot, cvaFn(props)])
+    return Object.fromEntries(result)
+  }
+
+  function raw(props) {
+    const result = slots.map(([slot, cvaFn]) => [slot, cvaFn.raw(props)])
     return Object.fromEntries(result)
   }
 
@@ -22,6 +27,7 @@ export function sva(config) {
 
   return Object.assign(svaFn, {
     __cva__: false,
+    raw,
     variantMap,
     variantKeys,
     splitVariantProps,
