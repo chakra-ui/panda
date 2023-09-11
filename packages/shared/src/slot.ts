9e96cf760dfc304112f3461a7158ea595d6f93dd
Segun Adebayo <joseshegs@gmail.com>
2023-09-11 22:47:25 +0100

    fix: slot recipe types

--- packages/shared/src/slot.ts
+++ packages/shared/src/slot.ts
@@ -1,13 +1,4 @@
-type Recipe = {
-  className?: string
-  slots: string[]
-  base?: any
-  variants?: Record<string, Record<string, Record<string, any>>>
-  defaultVariants?: Record<string, string>
-  compoundVariants?: any[]
-}
-
-export const getSlotRecipes = (recipe: Recipe) => {
+export const getSlotRecipes = (recipe: any): Record<string, any> => {
   const init = (slot: string) => ({
     className: [recipe.className, slot].filter(Boolean).join('__'),
     base: recipe.base?.[slot] ?? {},
@@ -16,10 +7,10 @@ export const getSlotRecipes = (recipe: Recipe) => {
     compoundVariants: recipe.compoundVariants ? getSlotCompoundVariant(recipe.compoundVariants, slot) : [],
   })
 
-  const recipeParts = recipe.slots.map((slot: any) => [slot, init(slot)])
+  const recipeParts = recipe.slots.map((slot: any) => [slot, init(slot)]) as [string, any][]
 
   for (const [variantsKey, variantsSpec] of Object.entries(recipe.variants ?? {})) {
-    for (const [variantKey, variantSpec] of Object.entries(variantsSpec)) {
+    for (const [variantKey, variantSpec] of Object.entries(variantsSpec as Record<string, any>)) {
       recipeParts.forEach(([slot, slotRecipe]) => {
         slotRecipe.variants[variantsKey] ??= {}
         slotRecipe.variants[variantsKey][variantKey] = variantSpec[slot] ?? {}
