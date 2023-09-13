0c87ef7050b46274dac9861eccf5926066e359f0
Segun Adebayo <joseshegs@gmail.com>
2023-09-13 19:03:49 +0100

    refactor: cli command flags

---
+++ packages/cli/src/types.ts
@@ -0,0 +1,69 @@
+import type { Config } from '@pandacss/types'
+
+export interface InitCommandFlags
+  extends Pick<Config, 'jsxFramework' | 'syntax' | 'cwd' | 'poll' | 'watch' | 'gitignore' | 'outExtension'> {
+  force?: boolean
+  postcss?: boolean
+  silent?: boolean
+  interactive?: boolean
+  config?: string
+}
+
+export interface CssGenCommandFlags {
+  silent?: boolean
+  clean?: boolean
+  outfile?: string
+  watch?: boolean
+  poll?: boolean
+  cwd?: string
+  config?: string
+  minify?: boolean
+}
+
+export interface StudioCommandFlags extends Pick<Config, 'cwd'> {
+  build?: boolean
+  preview?: boolean
+  config?: string
+  outdir?: string
+}
+
+export interface AnalyzeCommandFlags {
+  silent?: boolean
+  json?: string
+  cwd?: string
+  config?: string
+}
+
+export interface DebugCommandFlags {
+  silent?: boolean
+  dry?: boolean
+  outdir?: string
+  cwd?: string
+  config?: string
+}
+
+export interface ShipCommandFlags {
+  silent?: boolean
+  minify?: boolean
+  outfile?: string
+  cwd?: string
+  config?: string
+}
+
+export type CodegenCommandFlags = Pick<Config, 'cwd' | 'poll' | 'watch'> & {
+  clean?: boolean
+  silent?: boolean
+  config?: string
+}
+
+export type MainCommandFlags = Pick<Config, 'cwd' | 'poll' | 'watch'> & {
+  outdir?: string
+  minify?: boolean
+  config?: string
+  cwd: string
+  preflight?: boolean
+  silent?: boolean
+  exclude?: string[]
+  hash?: boolean
+  emitTokensOnly?: boolean
+}
