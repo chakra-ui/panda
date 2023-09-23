202d458349b06111b2db297d455270a8c34568db
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-09-23 19:26:15 +0200

    chore: config panda

--- codegen-sandbox/panda.config.ts
+++ codegen-sandbox/panda.config.ts
@@ -1,24 +1,44 @@
-import { defineConfig } from "@pandacss/dev"
+import { defineConfig } from '@pandacss/dev'
 
 export default defineConfig({
-    // Whether to use css reset
-    preflight: true,
-    
-    // Where to look for your css declarations
-    include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  // Whether to use css reset
+  preflight: true,
 
-    // Files to exclude
-    exclude: [],
+  // Where to look for your css declarations
+  include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
 
-    // Useful for theme customization
-    theme: {
-      extend: {}
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {},
+  },
+
+  // The output directory for your css system
+  outdir: 'styled-system',
+
+  // The JSX framework to use
+  jsxFramework: 'react',
+  globalCss: {
+    html: {
+      lineHeight: 1.5,
+      textRendering: 'optimizeLegibility',
+      MozOsxFontSmoothing: 'grayscale',
+      WebkitFontSmoothing: 'antialiased',
+      WebkitTextSizeAdjust: '100%',
+      height: '100%',
     },
-
-    // The output directory for your css system
-    outdir: "styled-system",
-    
- // The JSX framework to use
-jsxFramework: 'react',
-    
+    'body, #root': {
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: 'full',
+      height: '100%',
+      maxHeight: '100%',
+      _dark: {
+        colorScheme: 'dark',
+        bg: '#282828',
+      },
+    },
+  },
 })
--- codegen-sandbox/src/App.tsx
+++ codegen-sandbox/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { center } from '../styled-system/patterns'
+import { css } from '../styled-system/css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={center({ w: 'full', h: 'full' })}>
+      <div
+        className={css({
+          display: 'flex',
+          flexDir: 'column',
+          fontWeight: 'semibold',
+          color: 'blue.300',
+          textAlign: 'center',
+          textStyle: '4xl',
+        })}
+      >
+        <span>🐼</span>
+        <span>Hello from Panda</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
