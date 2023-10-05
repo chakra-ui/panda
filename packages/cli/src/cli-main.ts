f776864d12d9d02caf912feb7eb2d0407e6cd29d
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-10-05 15:23:08 +0200

    feat: dynamic import CLI source when in dev

--- packages/cli/src/cli-main.ts
+++ packages/cli/src/cli-main.ts
@@ -23,7 +23,6 @@ import { buildStudio, previewStudio, serveStudio } from '@pandacss/studio'
 import { cac } from 'cac'
 import { join, resolve } from 'pathe'
 import { debounce } from 'perfect-debounce'
-import updateNotifier from 'update-notifier'
 import { name, version } from '../package.json'
 import { cliInit } from './cli-init'
 import type {
@@ -37,8 +36,11 @@ import type {
   StudioCommandFlags,
 } from './types'
 
-export async function main() {
-  updateNotifier({ pkg: { name, version }, distTag: 'latest' }).notify()
+export async function main(isDev = false) {
+  // needed to fix SyntaxError: Identifier '__dirname' has already been declared
+  if (!isDev) {
+    require('update-notifier')({ pkg: { name, version }, distTag: 'latest' }).notify()
+  }
 
   const cli = cac('panda')
 
