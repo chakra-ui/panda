f799e106375a5245fbbbf29b4d91e45927c35e83
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-10-19 12:30:28 +0200

    opti: reduce outdir tokens.mjs size by recomputing obj just like in css.mjs and is-valid-prop.mjs

--- packages/generator/src/artifacts/js/token.ts
+++ packages/generator/src/artifacts/js/token.ts
@@ -3,19 +3,31 @@ import type { Context } from '../../engines'
 
 export function generateTokenJs(ctx: Context) {
   const { tokens } = ctx
-  const map = new Map<string, { value: string; variable: string }>()
 
-  tokens.allTokens.forEach((token) => {
-    const { varRef } = token.extensions
-    const value = token.isConditional ? varRef : token.value
-    map.set(token.name, { value, variable: varRef })
-  })
+  // encode token as:
+  // path[]variable[]value<!
 
-  const obj = Object.fromEntries(map)
+  // ex:
+  // { path: 'blurs.sm', value: '4px', variable: 'var(--inkeep-blurs-sm)', result: 'blurs.sm[]4px[]var(--inkeep-blurs-sm)<!' }
+  // { path: 'fonts.serif', value: 'ui-serif, Georgia, "Times New Roman"', variable: 'var(--fonts-serif)', result: 'fonts.serif[]ui-serif, Georgia, "Times New Roman"[]var(--fonts-serif)<!' }
+
+  const tokenList = tokens.allTokens
+    .map((token) => {
+      const { varRef } = token.extensions
+      const parts = [token.name, varRef]
+      if (!token.isConditional) parts.push(token.value)
+      return parts.join('[]')
+    })
+    .join('<!')
 
   return {
     js: outdent`
-  const tokens = ${JSON.stringify(obj, null, 2)}
+  const tokenList = \`${tokenList}\`;
+
+  const tokens = Object.fromEntries(tokenList.split('<!').map((token) => {
+    const [name, variable, value] = token.split('[]')
+    return [name, { value: value || variable, variable }]
+  }))
 
   export function token(path, fallback) {
     return tokens[path]?.value || fallback
