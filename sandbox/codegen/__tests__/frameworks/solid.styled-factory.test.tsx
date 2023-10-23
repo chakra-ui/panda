296d62b1b7b52823bee7d1eaacb5afc2d19b6d99
Alexandre Stahmer <47224540+astahmer@users.noreply.github.com>
2023-10-23 19:52:54 +0200

    fix(#1549): OmittedHTMLProps + jsxStyleProps minimal/none (#1552)

--- sandbox/codegen/__tests__/frameworks/solid.styled-factory.test.tsx
+++ sandbox/codegen/__tests__/frameworks/solid.styled-factory.test.tsx
@@ -163,6 +163,23 @@ describe('styled factory - cva', () => {
       </button>
     `)
   })
+
+  test('html props', () => {
+    const { container } = render(
+      <styled.div htmlWidth={123} height="123">
+        Click me
+      </styled.div>,
+    )
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <div
+        class="h_123"
+        width="123"
+      >
+        Click me
+      </div>
+    `)
+  })
 })
 
 describe('styled factory - button recipe', () => {
