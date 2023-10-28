8cd3b92bed14103c641b07b7cfe7258f24b196e7
anubra266 <anubra266@gmail.com>
2023-10-28 01:29:53 +0100

    fix(studio): display semantic colors correctly

--- packages/studio/src/components/semantic-color.tsx
+++ packages/studio/src/components/semantic-color.tsx
@@ -1,14 +1,26 @@
 import { Flex, panda } from '../../styled-system/jsx'
 import { ColorWrapper } from './color-wrapper'
+import context from '../lib/panda.context'
+
+const getSemanticColorValue = (variable: string): string => {
+  const name = variable.match(/var\(\s*--(.*?)\s*\)/)![1].replaceAll('-', '.')
+  const token = context.tokens.getByName(name)
+  if (token) return token.originalValue
+  const defaultToken = context.tokens.getByName(`${name}.default`)
+  return getSemanticColorValue(defaultToken?.value)
+}
 
 // remove initial underscore
 const cleanCondition = (condition: string) => condition.replace(/^_/, '')
 
 export function SemanticColorDisplay(props: { value: string; condition: string; token?: string }) {
   const { value, condition, token } = props
+
+  const tokenValue = getSemanticColorValue(value)
+
   return (
     <Flex direction="column" w="full">
-      <ColorWrapper height="12" style={{ background: value }}>
+      <ColorWrapper height="12" style={{ background: tokenValue }}>
         <panda.span
           fontWeight="medium"
           fontSize="sm"
@@ -26,7 +38,7 @@ export function SemanticColorDisplay(props: { value: string; condition: string; 
       </ColorWrapper>
       {token && <panda.div fontWeight="medium">{token}</panda.div>}
       <panda.div opacity="0.7" fontSize="sm" textTransform="uppercase">
-        {value}
+        {value} - {tokenValue}
       </panda.div>
     </Flex>
   )
