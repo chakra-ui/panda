ea525ae220a0f4efee7d446d1c74fa7372ccad7b
Segun Adebayo <joseshegs@gmail.com>
2023-11-01 17:40:03 +0000

    fix: astish

--- packages/shared/src/astish.ts
+++ packages/shared/src/astish.ts
@@ -1,11 +1,18 @@
 const newRule = /(?:([\u0080-\uFFFF\w-%@]+) *:? *([^{;]+?);|([^;}{]*?) *{)|(}\s*)/g
-const ruleClean = /\/\*[^]*?\*\/|\s\s+|\n/g
+const ruleClean = /\/\*[^]*?\*\/|  +/g
+const ruleNewline = /\n+/g
+const empty = ' '
 
 export const astish = (val: string, tree: any[] = [{}]): Record<string, any> => {
-  const block = newRule.exec((val ?? '').replace(ruleClean, ''))
-  if (!block) return tree[0]
-  if (block[4]) tree.shift()
-  else if (block[3]) tree.unshift((tree[0][block[3]] = tree[0][block[3]] || {}))
-  else tree[0][block[1]] = block[2]
-  return astish(val, tree)
+  if (!val) return tree[0]
+  let block, left
+  while ((block = newRule.exec(val.replace(ruleClean, '')))) {
+    if (block[4]) tree.shift()
+    else if (block[3]) {
+      left = block[3].replace(ruleNewline, empty).trim()
+      tree.unshift((tree[0][left] = tree[0][left] || {}))
+    } else tree[0][block[1]] = block[2].replace(ruleNewline, empty).trim()
+  }
+
+  return tree[0]
 }
