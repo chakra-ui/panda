ac2244ee53004b6723f54a0ccc34cd092a3b9b58
anubra266 <anubra266@gmail.com>
2023-11-26 17:09:15 +0100

    Add responsive breakpoints to playground

--- playground/src/components/icons.tsx
+++ playground/src/components/icons.tsx
@@ -132,3 +132,21 @@ export function ErrorIcon() {
     </svg>
   )
 }
+
+export function ChevronDownIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m6 9 6 6 6-6" />
+    </svg>
+  )
+}
