4ba982f301e003048a63edf3632b9b83a9a89b42
Alexandre Stahmer <47224540+astahmer@users.noreply.github.com>
2023-11-30 15:33:50 +0100

    fix: token fn in at rules (#1731)

--- packages/parser/__tests__/output.test.ts
+++ packages/parser/__tests__/output.test.ts
@@ -2625,6 +2625,68 @@ describe('extract to css output pipeline', () => {
       }"
     `)
   })
+
+  test('token fn in at-rules', () => {
+    const code = `
+    import { css } from '.panda/css';
+
+    css({
+      '@container (min-width: token(sizes.xl))': {
+        color: 'green.300',
+      },
+      '@media (min-width: token(sizes.2xl))': {
+        color: 'red.300',
+      },
+      "@container (min-width: token(sizes.4xl, 1280px))": {
+        display: "flex"
+      }
+    })
+     `
+    const result = run(code)
+    expect(result.json).toMatchInlineSnapshot(`
+      [
+        {
+          "data": [
+            {
+              "@container (min-width: token(sizes.4xl, 1280px))": {
+                "display": "flex",
+              },
+              "@container (min-width: token(sizes.xl))": {
+                "color": "green.300",
+              },
+              "@media (min-width: token(sizes.2xl))": {
+                "color": "red.300",
+              },
+            },
+          ],
+          "name": "css",
+          "type": "object",
+        },
+      ]
+    `)
+
+    expect(result.css).toMatchInlineSnapshot(`
+      "@layer utilities {
+        @container (min-width: 36rem) {
+          .\\\\[\\\\@container_\\\\(min-width\\\\:_token\\\\(sizes\\\\.xl\\\\)\\\\)\\\\]\\\\:text_green\\\\.300 {
+            color: var(--colors-green-300)
+          }
+              }
+
+        @container (min-width: 56rem) {
+          .\\\\[\\\\@container_\\\\(min-width\\\\:_token\\\\(sizes\\\\.4xl\\\\,_1280px\\\\)\\\\)\\\\]\\\\:d_flex {
+            display: flex
+          }
+              }
+
+        @media (min-width: 42rem) {
+          .\\\\[\\\\@media_\\\\(min-width\\\\:_token\\\\(sizes\\\\.2xl\\\\)\\\\)\\\\]\\\\:text_red\\\\.300 {
+            color: var(--colors-red-300)
+          }
+              }
+      }"
+    `)
+  })
 })
 
 describe('preset patterns', () => {
