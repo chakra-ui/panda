5c3ea584e3be6389236bcdfaefa01fad7c7be75f
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-12-03 18:12:24 +0100

    fix(vue): styled classes merging

---
+++ sandbox/nuxt/components/Styled.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { styled } from '../styled-system/jsx'
+
+const StyledButton = styled('button', {
+  base: {
+    bgColor: 'black',
+  },
+  variants: {
+    vColor: {
+      default: {
+        bgColor: 'pink',
+      },
+      error: {
+        bgColor: 'red',
+      },
+      warning: {
+        bgColor: 'yellow',
+      },
+      info: {
+        bgColor: 'cyan',
+      },
+      secondary: {
+        bgColor: 'green',
+      },
+    },
+  },
+})
+</script>
+<template>
+  <StyledButton id="test" class="test">
+    <slot></slot>
+  </StyledButton>
+</template>
