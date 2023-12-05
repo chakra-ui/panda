7ae0364c5ef271fe4164b2c18ce30960c463e992
Segun Adebayo <joseshegs@gmail.com>
2023-12-05 15:39:19 +0000

    test: update snap

--- packages/generator/__tests__/setup-artifacts.test.ts
+++ packages/generator/__tests__/setup-artifacts.test.ts
@@ -130,13 +130,9 @@ describe('setup-artifacts', () => {
           "/helpers.mjs",
         ],
         [
-          "tokens/index.css",
           "tokens/index.d.ts",
           "tokens/index.mjs",
           "tokens/tokens.d.ts",
-        ],
-        [
-          "tokens/keyframes.css",
         ],
         [],
         [
@@ -248,13 +244,6 @@ describe('setup-artifacts', () => {
         [
           "css/index.mjs",
           "css/index.d.ts",
-        ],
-        [],
-        [
-          "/global.css",
-        ],
-        [
-          "/static.css",
         ],
         [],
       ]
