9244f8cbec6850a7e25c4037200288617198801b
anubra266 <anubra266@gmail.com>
2023-12-05 01:49:39 +0100

    Throw 404s

--- playground/src/app/[id]/[id2]/page.tsx
+++ playground/src/app/[id]/[id2]/page.tsx
@@ -1,21 +1,30 @@
 import { Playground } from '@/src/components/Playground'
 import { prisma } from '../../../client/prisma'
 import { parseState } from '@/src/lib/parse-state'
+import { notFound } from 'next/navigation'
 
 const Page = async (props: any) => {
   const {
     params: { id, id2 },
   } = props
 
-  const initialState = await prisma.session.findFirst({
-    where: { id },
-    select: { code: true, css: true, config: true },
-  })
+  const initialState = await prisma.session
+    .findFirst({
+      where: { id },
+      select: { code: true, css: true, config: true },
+    })
+    .catch(() => {
+      return notFound()
+    })
 
-  const diffState = await prisma.session.findFirst({
-    where: { id: id2 },
-    select: { code: true, css: true, config: true },
-  })
+  const diffState = await prisma.session
+    .findFirst({
+      where: { id: id2 },
+      select: { code: true, css: true, config: true },
+    })
+    .catch(() => {
+      return notFound()
+    })
 
   return <Playground initialState={parseState(initialState)} diffState={parseState(diffState)} />
 }
