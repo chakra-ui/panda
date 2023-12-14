079ad30632ae7123808099f4cdec2b7a40c3d629
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-12-14 14:44:21 +0100

    test: add failing test

--- packages/parser/__tests__/output.test.ts
+++ packages/parser/__tests__/output.test.ts
@@ -2822,7 +2822,7 @@ describe('extract to css output pipeline', () => {
     expect(result.css).toMatchInlineSnapshot('""')
   })
 
-  test('strictTokens arbitrary value escape hatch', () => {
+  test.only('strictTokens arbitrary value escape hatch', () => {
     const code = `
     import { css } from '.panda/css';
 
@@ -2830,6 +2830,8 @@ describe('extract to css output pipeline', () => {
       color: '[#fff]',
       bg: 'red.300',
       bgColor: '[rgb(51 155 240)]',
+      outlineColor: '[rgb(51 155 240)!]',
+      borderColor: '[rgb(51 155 240)!important]',
     })
      `
     const result = parseAndExtract(code, { strictTokens: true })
@@ -2840,7 +2842,9 @@ describe('extract to css output pipeline', () => {
             {
               "bg": "red.300",
               "bgColor": "[rgb(51 155 240)]",
+              "borderColor": "[rgb(51 155 240)!important]",
               "color": "[#fff]",
+              "outlineColor": "[rgb(51 155 240)!]",
             },
           ],
           "name": "css",
@@ -2861,6 +2865,14 @@ describe('extract to css output pipeline', () => {
 
         .bg_\\\\[rgb\\\\(51_155_240\\\\)\\\\] {
           background-color: rgb(51 155 240)
+          }
+
+        .ring_\\\\[rgb\\\\(51_155_240\\\\)\\\\!\\\\] {
+          outline-color: rgb(51 155 240)!
+          }
+
+        .border_\\\\[rgb\\\\(51_155_240\\\\)\\\\!important\\\\] {
+          border-color: rgb(51 155 240) !important
           }
       }"
     `)
