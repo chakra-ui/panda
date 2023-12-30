a5d27c2db9d01681b6998aa222bf6bbe27b57380
anubra266 <anubra266@gmail.com>
2023-12-30 03:16:30 +0100

    Only use worker when there are presets

--- playground/src/hooks/useConfig.ts
+++ playground/src/hooks/useConfig.ts
@@ -1,9 +1,12 @@
 import { evalConfig } from '@/src/lib/config/eval-config'
+import { getImports } from '@/src/lib/config/get-imports'
 import { Config } from '@pandacss/types'
 import { useEffect, useRef, useState } from 'react'
 import { useUpdateEffect } from 'usehooks-ts'
 
 export const useConfig = (_config: string) => {
+  const hasPresets = getImports(_config).length || evalConfig(_config)?.presets?.length
+
   const [config, setConfig] = useState<Config | null>(evalConfig(_config))
   const [error, setError] = useState<Error | null>(null)
 
@@ -14,7 +17,10 @@ export const useConfig = (_config: string) => {
   useEffect(() => {
     compileWorkerRef.current = new Worker(new URL('../lib/config/compile.worker.ts', import.meta.url))
 
-    compileWorkerRef.current?.postMessage(_config)
+    if (hasPresets) compileWorkerRef.current?.postMessage(_config)
+    else {
+      setIsLoading(false)
+    }
 
     compileWorkerRef.current.onmessage = (event: MessageEvent<{ config: string; error: any }>) => {
       setIsLoading(false)
@@ -33,8 +39,13 @@ export const useConfig = (_config: string) => {
   }, [])
 
   useUpdateEffect(() => {
-    setIsLoading(true)
-    compileWorkerRef.current?.postMessage(_config)
+    if (hasPresets) {
+      compileWorkerRef.current?.postMessage(_config)
+      setIsLoading(true)
+    } else {
+      const newConfig = evalConfig(_config)
+      if (newConfig) setConfig(newConfig)
+    }
   }, [_config])
 
   return { config, isLoading, error }
