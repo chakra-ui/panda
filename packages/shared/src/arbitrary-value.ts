657ca5da08d1d2db396952ab054ee055e9ece735
Abraham <anubra266@gmail.com>
2024-01-06 20:08:59 +0100

    Fix escape hash clash (#1949)
    * Fix escape hash clash
    
    * chore: add changeset
    
    * chore: add tests

--- packages/shared/src/arbitrary-value.ts
+++ packages/shared/src/arbitrary-value.ts
@@ -2,7 +2,25 @@ export const getArbitraryValue = (value: string) => {
   if (!value) return value
 
   if (value[0] === '[' && value[value.length - 1] === ']') {
-    return value.slice(1, -1)
+    const innerValue = value.slice(1, -1)
+    let bracketCount = 0
+
+    for (let i = 0; i < innerValue.length; i++) {
+      if (innerValue[i] === '[') {
+        bracketCount++
+      } else if (innerValue[i] === ']') {
+        if (bracketCount === 0) {
+          // Unmatched closing bracket found
+          return value
+        }
+        bracketCount--
+      }
+    }
+
+    if (bracketCount === 0) {
+      // All brackets are balanced
+      return innerValue
+    }
   }
 
   return value
