0889bd498de4dc156d1ff6f9721b1ee1698db2a7
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-01-07 11:00:42 +0100

    BREAKING: remove @pandacss/astro in favor of just using @pandacss/postcss

--- packages/astro/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig({
-  entry: ['./src/index.ts'],
-  format: ['esm'],
-  splitting: false,
-  clean: true,
-  external: ['@pandacss/dev/postcss'],
-})
