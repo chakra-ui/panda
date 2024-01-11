8fe91c1d04378a80f664c414156b12c43b153b59
Segun Adebayo <joseshegs@gmail.com>
2024-01-11 16:55:25 +0000

    revert: prettify

--- packages/core/__tests__/sort-css.test.ts
+++ packages/core/__tests__/sort-css.test.ts
@@ -1,9 +1,10 @@
 import postcss from 'postcss'
 import { describe, expect, test } from 'vitest'
+import prettify from '../src/plugins/prettify'
 import sortCss from '../src/plugins/sort-css'
 
 function run(code: string) {
-  return postcss([sortCss()]).process(code, { from: undefined }).css
+  return postcss([sortCss(), prettify()]).process(code, { from: undefined }).css
 }
 
 describe('sort css', () => {
@@ -21,16 +22,16 @@ describe('sort css', () => {
         width: var(--sizes-5)
       }`),
     ).toMatchInlineSnapshot(`
-      "
-            .justify_center {
-              justify-content: center
+      ".justify_center {
+        justify-content: center
             }
 
-            .w_5 {
-              width: var(--sizes-5)
+      .w_5 {
+        width: var(--sizes-5)
             }
-              .hover:text_red.200:where(:hover, [data-hover]) {
-                color: var(--colors-red-200)
+
+      .hover:text_red.200:where(:hover, [data-hover]) {
+        color: var(--colors-red-200)
               }"
     `)
   })
@@ -86,52 +87,53 @@ describe('sort css', () => {
         }
       }`),
     ).toMatchInlineSnapshot(`
-      "
-            @layer utilities {
-              .d_flex {
-                display: flex
+      "@layer utilities {
+        .d_flex {
+          display: flex
               }
 
-              .items_center {
-                align-items: center
+        .items_center {
+          align-items: center
               }
 
-              .justify_center {
-                justify-content: center
+        .justify_center {
+          justify-content: center
               }
 
-              .w_5 {
-                width: var(--sizes-5)
+        .w_5 {
+          width: var(--sizes-5)
               }
 
-              .h_6 {
-                height: var(--sizes-6)
+        .h_6 {
+          height: var(--sizes-6)
               }
 
-              .w_12 {
-                width: var(--sizes-12)
+        .w_12 {
+          width: var(--sizes-12)
               }
 
-              .h_12 {
-                height: var(--sizes-12)
+        .h_12 {
+          height: var(--sizes-12)
               }
 
-              .animation-name_red {
-                animation-name: red
+        .animation-name_red {
+          animation-name: red
               }
 
-              .rounded_8px {
-                border-radius: 8px
+        .rounded_8px {
+          border-radius: 8px
               }
 
-              .rounded_999px {
-                border-radius: 999px
+        .rounded_999px {
+          border-radius: 999px
               }
-                .hover:text_red.200:where(:hover, [data-hover]) {
-                  color: var(--colors-red-200)
+
+        .hover:text_red.200:where(:hover, [data-hover]) {
+          color: var(--colors-red-200)
                 }
-                .hover:bg_red.50:where(:hover, [data-hover]) {
-                  background: var(--colors-red-50)
+
+        .hover:bg_red.50:where(:hover, [data-hover]) {
+          background: var(--colors-red-50)
                 }
             }"
     `)
