dd839e23a0f6501825b51fb0b3a3564ccb4778af
Segun Adebayo <joseshegs@gmail.com>
2024-01-18 18:05:20 +0000

    fix: ts config paths hmr

---
+++ packages/node/src/load-tsconfig.ts
@@ -0,0 +1,30 @@
+import { convertTsPathsToRegexes } from '@pandacss/config'
+import type { LoadConfigResult, LoadTsConfigResult } from '@pandacss/types'
+import { parse } from 'tsconfck'
+
+export async function loadTsConfig(conf: LoadConfigResult, cwd: string): Promise<LoadTsConfigResult | undefined> {
+  const tsconfigResult = await parse(conf.path, {
+    root: cwd,
+    resolveWithEmptyIfConfigNotFound: true,
+  })
+
+  if (!tsconfigResult) return
+
+  const { tsconfig, tsconfigFile } = tsconfigResult
+  const { compilerOptions } = tsconfig
+
+  const result: LoadTsConfigResult = {
+    tsconfig,
+    tsconfigFile,
+  }
+
+  if (compilerOptions?.paths) {
+    const baseUrl = compilerOptions.baseUrl
+    result.tsOptions = {
+      baseUrl,
+      pathMappings: convertTsPathsToRegexes(compilerOptions.paths, baseUrl ?? cwd),
+    }
+  }
+
+  return result
+}
