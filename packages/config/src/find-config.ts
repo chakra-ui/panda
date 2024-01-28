df5c4a326d517fa6676fd6afaedc032cf497d0ad
Segun Adebayo <joseshegs@gmail.com>
2024-01-28 18:15:00 +0000

    refactor: some stuff

--- packages/config/src/find-config.ts
+++ packages/config/src/find-config.ts
@@ -1,12 +1,8 @@
 import { PandaError } from '@pandacss/shared'
 import findUp from 'escalade/sync'
 import { resolve } from 'path'
+import { isPandaConfig } from './is-panda-config'
 import type { ConfigFileOptions } from './types'
-
-const configs = ['.ts', '.js', '.mts', '.mjs', '.cts', '.cjs']
-const pandaConfigRegex = new RegExp(`panda.config(${configs.join('|')})$`)
-
-const isPandaConfig = (file: string) => pandaConfigRegex.test(file)
 
 export function findConfig(options: Partial<ConfigFileOptions>): string {
   const { cwd = process.cwd(), file } = options
