54ff4dfbaf7afb64ebb88f03a6d09c3dc11eb949
Julien Ripouteau <julien@ripouteau.com>
2024-02-02 13:21:44 +0100

    refactor: remove circular dependency

--- packages/postcss/__tests__/samples/panda.config.cjs
+++ packages/postcss/__tests__/samples/panda.config.cjs
@@ -1,9 +1,7 @@
-const { defineConfig } = require('@pandacss/dev')
-
-module.exports = defineConfig({
+module.exports = {
   preflight: true,
   presets: ['@pandacss/dev/presets'],
   include: ['./src/**/*.{ts,tsx,jsx}', './src/**/*.{css,pcss}'],
   exclude: [],
   jsxFramework: 'react',
-})
+}
