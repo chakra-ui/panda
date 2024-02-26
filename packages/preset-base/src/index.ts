a1a359faff36cc018f0c5b63d7f24bbc9909505e
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-02-26 23:26:43 +0100

    chore: conditions.getSortedKeys / remove utilitiesGroups

--- packages/preset-base/src/index.ts
+++ packages/preset-base/src/index.ts
@@ -1,7 +1,7 @@
 import type { Preset } from '@pandacss/types'
 
 import { conditions } from './conditions'
-import { utilities, utilitiesGroups } from './utilities'
+import { utilities } from './utilities'
 import { patterns } from './patterns'
 
 const definePreset = <T extends Preset>(preset: T) => preset
@@ -13,5 +13,3 @@ export const preset = definePreset({
 })
 
 export default preset
-
-export { utilitiesGroups }
