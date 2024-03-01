01bdf344050a71e3078e5f26cba7c5186021e77f
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-03-01 16:53:04 +0100

    fix: splitCssProps typings "Expression produces a union type that is too complex to represent"

---
+++ .changeset/sixty-trains-mix.md
@@ -0,0 +1,5 @@
+---
+"@pandacss/generator": patch
+---
+
+Fix `splitCssProps` typings, it would sometimes throw `Expression produces a union type that is too complex to represent"`
--- packages/studio/styled-system/jsx/is-valid-prop.d.ts
+++ packages/studio/styled-system/jsx/is-valid-prop.d.ts
@@ -4,9 +4,8 @@ import type { DistributiveOmit, HTMLPandaProps, JsxStyleProps, Pretty } from '..
 declare const isCssProperty: (value: string) => boolean;
 
 type CssPropKey = keyof JsxStyleProps
-type PickedCssProps<T> = Pretty<Pick<T, CssPropKey>>
 type OmittedCssProps<T> = Pretty<DistributiveOmit<T, CssPropKey>>
 
-declare const splitCssProps: <T>(props: T) => [PickedCssProps<T>, OmittedCssProps<T>]
+declare const splitCssProps: <T>(props: T) => [JsxStyleProps, OmittedCssProps<T>]
 
 export { isCssProperty, splitCssProps };
