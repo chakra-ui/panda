67fc226818a1af84ce96c796c0ca501ca27c5b40
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-03-06 23:05:53 +0100

    fix: color-mix in semanticTokens conditions

--- packages/generator/__tests__/generate-token.test.ts
+++ packages/generator/__tests__/generate-token.test.ts
@@ -833,4 +833,62 @@ describe('generator', () => {
       }"
     `)
   })
+
+  test('color-mix in semanticTokens conditions', () => {
+    const css = tokenCss(<any>{
+      dependencies: [],
+      config: {
+        cwd: '',
+        include: [],
+        conditions: {
+          light: '.light &',
+          dark: '.dark &',
+        },
+        theme: {
+          tokens: {
+            colors: {
+              blue: { 500: { value: 'blue' } },
+              green: { 500: { value: 'green' } },
+            },
+            opacity: {
+              half: { value: 0.5 },
+            },
+          },
+          semanticTokens: {
+            colors: {
+              secondary: {
+                value: {
+                  base: 'red',
+                  _light: '{colors.blue.500/32}',
+                  _dark: '{colors.green.500/half}',
+                },
+              },
+            },
+          },
+        },
+        outdir: '',
+      },
+      path: '',
+      hooks: {},
+    })
+
+    expect(css).toMatchInlineSnapshot(`
+      "@layer tokens {
+        :where(:root, :host) {
+          --colors-blue-500: blue;
+          --colors-green-500: green;
+          --opacity-half: 0.5;
+          --colors-secondary: red;
+      }
+
+        .light {
+          --colors-secondary: color-mix(in srgb, var(--colors-blue-500) 32%, transparent)
+      }
+
+        .dark {
+          --colors-secondary: color-mix(in srgb, var(--colors-green-500) 50%, transparent)
+      }
+      }"
+    `)
+  })
 })
