5031e6e62696a4922e7b596ec9e36794b5532646
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-03-14 22:40:55 +0100

    fix: color opacity modifier with semanticTokens

--- packages/token-dictionary/__tests__/color-mix.test.ts
+++ packages/token-dictionary/__tests__/color-mix.test.ts
@@ -26,4 +26,54 @@ test('color-mix', () => {
   expect(dictionary.expandReferenceInValue('{colors.border/half}')).toMatchInlineSnapshot(
     `"color-mix(in srgb, var(--colors-border) 50%, transparent)"`,
   )
+
+  expect(dictionary.view.vars).toMatchInlineSnapshot(`
+    Map {
+      "base" => Map {
+        "--colors-pink" => "#ff00ff",
+        "--colors-border" => "color-mix(in srgb, var(--colors-pink) 30%, transparent)",
+        "--colors-ref" => "color-mix(in srgb, var(--colors-border) 40%, transparent)",
+        "--opacity-half" => 0.5,
+      },
+    }
+  `)
+})
+
+test('color-mix with semanticTokens', () => {
+  const dictionary = new TokenDictionary({
+    tokens: {
+      colors: {
+        black: { value: 'black' },
+        white: { value: 'white' },
+      },
+    },
+    semanticTokens: {
+      colors: {
+        fg: {
+          default: {
+            value: { base: '{colors.black/87}', _dark: '{colors.white}' },
+          },
+        },
+      },
+    },
+  })
+
+  dictionary.init()
+
+  expect(dictionary.expandReferenceInValue('{colors.black/87}')).toMatchInlineSnapshot(
+    `"color-mix(in srgb, var(--colors-black) 87%, transparent)"`,
+  )
+
+  expect(dictionary.view.vars).toMatchInlineSnapshot(`
+    Map {
+      "base" => Map {
+        "--colors-black" => "black",
+        "--colors-white" => "white",
+        "--colors-fg-default" => "color-mix(in srgb, var(--colors-black) 87%, transparent)",
+      },
+      "_dark:value" => Map {
+        "--colors-fg-default" => "var(--colors-white)",
+      },
+    }
+  `)
 })
