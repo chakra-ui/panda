220e3232c5f14e226c16f6e307d140484415b783
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-04-11 11:57:39 +0200

    wip

--- packages/shared/src/cache-map.ts
+++ packages/shared/src/cache-map.ts
@@ -1,4 +1,4 @@
-export class CacheMap<K, V> {
+export class CacheMap<K, V> implements Map<K, V> {
   private cache = new Map<K, V>()
   private previousCache = new Map<K, V>()
   private cacheSize = 0
@@ -8,6 +8,7 @@ export class CacheMap<K, V> {
   private update = (key: K, value: V): void => {
     this.cache.set(key, value)
     this.cacheSize++
+    console.log(key, this.cacheSize, this.maxCacheSize)
 
     if (this.cacheSize > this.maxCacheSize) {
       this.cacheSize = 0
@@ -25,12 +26,14 @@ export class CacheMap<K, V> {
     }
   }
 
-  set = (key: K, value: V): void => {
+  set = (key: K, value: V) => {
     if (this.cache.has(key)) {
       this.cache.set(key, value)
     } else {
       this.update(key, value)
     }
+
+    return this
   }
 
   has = (key: K): boolean => {
@@ -41,9 +44,36 @@ export class CacheMap<K, V> {
     return this.cache.delete(key)
   }
 
-  size = (): number => {
+  get size() {
     return this.cache.size
   }
+
+  clear = () => {
+    this.cache.clear()
+    this.previousCache.clear()
+  }
+
+  forEach = (fn: (value: V, key: K, map: Map<K, V>) => void) => {
+    this.cache.forEach(fn)
+  }
+
+  entries = () => {
+    return this.cache.entries()
+  }
+
+  keys = () => {
+    return this.cache.keys()
+  }
+
+  values = () => {
+    return this.cache.values()
+  };
+
+  [Symbol.iterator] = () => {
+    return this.cache[Symbol.iterator]()
+  };
+
+  [Symbol.toStringTag] = 'CacheMap'
 
   toJSON = () => {
     return this.cache
