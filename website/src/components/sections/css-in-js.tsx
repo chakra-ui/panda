9a7f4616d16bf814909f4cd122246ed13845a2b8
Matthew <matthewajustice@gmail.com>
2024-04-25 15:43:09 -0400

    refactor/alignment-correction: Adjusted the maxW prop of the mapped Stack in css-in-js.tsx to fix the off center alignment

--- website/src/components/sections/css-in-js.tsx
+++ website/src/components/sections/css-in-js.tsx
@@ -134,7 +134,7 @@ export const SectionCssInJS = () => {
 
             <Stack
               direction={{ base: 'column', lg: 'row' }}
-              align={{ base: 'center', lg: 'center' }}
+              align={{ base: 'center', lg: 'flex-start' }}
               justify="space-between"
               w="100%"
               gap="8"
