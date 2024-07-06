50686db186735518c7e6d42e3d4823384917ee98
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-07-06 15:11:47 +0200

    chore: add turbo

--- sandbox/runtime-perf/src/components/profiler.tsx
+++ sandbox/runtime-perf/src/components/profiler.tsx
@@ -1,51 +1,40 @@
-import {
-  Fragment,
-  Profiler as ReactProfiler,
-  useState,
-  useCallback,
-} from "react";
+// @ts-nocheck
+import { Fragment, Profiler as ReactProfiler, useState, useCallback } from 'react'
 
-function ProfilerResult(
-  id,
-  phase,
-  actualDuration,
-  baseDuration,
-  startTime,
-  commitTime
-) {
-  this.id = id;
-  this.phase = phase;
-  this.actualDuration = actualDuration;
-  this.baseDuration = baseDuration;
-  this.startTime = startTime;
-  this.commitTime = commitTime;
+function ProfilerResult(id, phase, actualDuration, baseDuration, startTime, commitTime) {
+  this.id = id
+  this.phase = phase
+  this.actualDuration = actualDuration
+  this.baseDuration = baseDuration
+  this.startTime = startTime
+  this.commitTime = commitTime
 }
 
-const entries = [];
+const entries = []
 
 const profilerResults = function (...args) {
-  entries.push(new ProfilerResult(...args));
-  console.group("Profiler");
-  console.table(entries);
-  console.groupEnd();
-};
+  entries.push(new ProfilerResult(...args))
+  console.group('Profiler')
+  console.table(entries)
+  console.groupEnd()
+}
 
 export const Profiler = ({ children, id, name, onRerender }) => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0)
 
   const onClick = useCallback(() => {
-    setValue((value) => value + 1);
-    onRerender(value);
-  }, [value]);
+    setValue((value) => value + 1)
+    onRerender(value)
+  }, [value])
 
   return (
     <Fragment>
       <h1>Panda</h1>
       <button onClick={onClick}>Force Rerender</button>
-      <hr style={{ margin: "24px 0" }} />
+      <hr style={{ margin: '24px 0' }} />
       <ReactProfiler onRender={profilerResults} id={id}>
         {children}
       </ReactProfiler>
     </Fragment>
-  );
-};
+  )
+}
