a02a656d8aa26815085969396fd7cee51b51a8f2
Segun Adebayo <joseshegs@gmail.com>
2024-12-24 19:02:42 +0000

    docs: fix

--- website/.eslintrc.cjs
+++ website/.eslintrc.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['plugin:@pandacss/recommended'],
   ignorePatterns: ['styled-system', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   rules: {}
