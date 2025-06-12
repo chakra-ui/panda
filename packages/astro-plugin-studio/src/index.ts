0b158e6430301fa494c65e1f480d1d33935d6907
Segun Adebayo <joseshegs@gmail.com>
2025-06-12 17:38:59 +0100

    fix: studio composition styles

--- packages/astro-plugin-studio/src/index.ts
+++ packages/astro-plugin-studio/src/index.ts
@@ -1,4 +1,5 @@
 import { loadConfigAndCreateContext, type PandaContext } from '@pandacss/node'
+import { stringify as stringifyPanda } from '@pandacss/core'
 import type { AstroIntegration } from 'astro'
 import { stringify } from 'javascript-stringify'
 import type { PluginOption } from 'vite'
@@ -8,9 +9,19 @@ const resolvedVirtualModuleId = '\0' + virtualModuleId
 
 function vitePlugin(configPath: string): PluginOption {
   let config: PandaContext['config']
+  const textStyleMap: Record<string, string> = {}
+  const layerStyleMap: Record<string, string> = {}
 
   async function loadPandaConfig() {
     const ctx = await loadConfigAndCreateContext({ configPath })
+    for (const key of Object.keys(ctx.config.theme?.textStyles ?? {})) {
+      const utility = ctx.utility.transform('textStyle', key)
+      textStyleMap[key] = stringifyPanda(utility.styles)
+    }
+    for (const key of Object.keys(ctx.config.theme?.layerStyles ?? {})) {
+      const utility = ctx.utility.transform('layerStyle', key)
+      layerStyleMap[key] = stringifyPanda(utility.styles)
+    }
     config = ctx.config
   }
 
@@ -37,7 +48,11 @@ function vitePlugin(configPath: string): PluginOption {
       if (id === resolvedVirtualModuleId) {
         await loadPandaConfig()
         return {
-          code: `export const config = ${stringify(config)}`,
+          code: [
+            `export const config = ${stringify(config)}`,
+            `export const textStyles = ${stringify(textStyleMap)}`,
+            `export const layerStyles = ${stringify(layerStyleMap)}`,
+          ].join('\n\n'),
         }
       }
     },
