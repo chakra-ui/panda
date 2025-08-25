d4d7fa1ed8d61843eda9d754c97377490a12ef1b
Segun Adebayo <joseshegs@gmail.com>
2025-08-25 17:13:50 +0100

    fix: typecheck

--- packages/core/src/selector.ts
+++ packages/core/src/selector.ts
@@ -26,3 +26,38 @@ export function extractParentSelectors(selector: string) {
 
   return result.size > 1 ? `:where(${finalized})` : finalized
 }
+
+export function extractTrailingPseudos(selector: string): [string, string] | [null, string] {
+  const ast = parser((selectors) => selectors).astSync(selector)
+
+  const matrix: any[][] = []
+
+  for (const [i, sel] of ast.nodes.entries()) {
+    for (const [j, child] of [...sel.nodes].reverse().entries()) {
+      if (child.type !== 'pseudo' || !child.value.startsWith('::')) {
+        break
+      }
+
+      matrix[j] = matrix[j] || []
+      matrix[j][i] = child
+    }
+  }
+
+  const trailingPseudos = parser.selector({ value: '' })
+
+  for (const pseudos of matrix) {
+    if (!pseudos) continue
+
+    const values = new Set(pseudos.map((p: any) => p.value))
+    if (values.size > 1) break
+
+    pseudos.forEach((pseudo: any) => pseudo.remove())
+    trailingPseudos.prepend(pseudos[0])
+  }
+
+  if (trailingPseudos.nodes.length) {
+    return [trailingPseudos.toString(), ast.toString()]
+  }
+
+  return [null, selector]
+}
